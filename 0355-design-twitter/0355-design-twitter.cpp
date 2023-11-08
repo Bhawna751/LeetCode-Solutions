f5f56a3856617e8c81b75cbe5ab2bfb50cc27db0
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2023-11-08 15:16:22 +0530

    Time: 4 ms (23.03%), Space: 7.7 MB (24.31%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -5,23 +5,22 @@ private:
    long long time; 
 
 public:
-/** Initialize your data structure here. */
+
 Twitter() {
     time = 0;
 }
 
-/** Compose a new tweet. */
+
 void postTweet(int userId, int tweetId) {
     t[userId].push_back({time++, tweetId});
 }
 
-/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
 vector<int> getNewsFeed(int userId) {
     priority_queue<pair<int, int>> maxHeap; 
     for (auto it=t[userId].begin();it!=t[userId].end();++it)
         maxHeap.push(*it);
     for (auto it1=fo[userId].begin();it1!=fo[userId].end();++it1){
-        int usr = *it1; // get target user
+        int usr = *it1; 
         for (auto it2=t[usr].begin();it2!=t[usr].end();++it2)
             maxHeap.push(*it2);
     }   
@@ -34,13 +33,11 @@ vector<int> getNewsFeed(int userId) {
     return res;
 }
 
-/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
 void follow(int followerId, int followeeId) {
     if (followerId != followeeId)
         fo[followerId].insert(followeeId);
 }
 
-/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
 void unfollow(int followerId, int followeeId) {
     fo[followerId].erase(followeeId);
 }
