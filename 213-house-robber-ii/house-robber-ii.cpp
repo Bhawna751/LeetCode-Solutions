8737867ffa44c3fb935697ab4423f0191e3d1821
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-01-30 05:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (14.45%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxAdjacent(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        for (int i = 1; i < n; i++) {
+            int take = nums[i];
+            if (i > 1)
+                take += prev2;
+            int nottake = 0 + prev;
+            int cur_i = max(take, nottake);
+            prev2 = prev;
+            prev = cur_i;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        vector<int> temp1, temp2;
+        int n = nums.size();
+        if(n==1)return nums[0];
+        for (int i = 0; i < n; i++) {
+            if(i!=0)temp1.push_back(nums[i]);
+            if(i!=n-1)temp2.push_back(nums[i]);
+        }
+        return max(maxAdjacent(temp1),maxAdjacent(temp2));
+    }
+
+};
