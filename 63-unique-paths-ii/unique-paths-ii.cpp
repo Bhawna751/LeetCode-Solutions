5196755b008de450be955c90e804046077fdb1c7
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-02-02 19:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (18.33%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -9,7 +9,7 @@ public:
         dp[pre][0] = -1;
         for (int i = 0; i < n; i++) {
             dp[curr][0] = obstacleGrid[i][0] == 1 ? 0 : dp[pre][0];
-            for (int j = 1; j < obstacleGrid[i].size(); j++)
+            for (int j = 1; j < m; j++)
                 dp[curr][j] = obstacleGrid[i][j] == 1 ? 0 : dp[curr][j - 1] + dp[pre][j];
             
             curr = (curr + 1) % 2;
