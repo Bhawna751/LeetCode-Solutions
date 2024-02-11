b156774d429225939f6054272babe3617bf1ae46
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-02-11 19:56:54 +0530

    Time: 21 ms (77.25%) | Memory: 25.4 MB (38.48%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -5,8 +5,7 @@ public:
      int n = s1.size();
         int m = s2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++)dp[i][0]=0;
-        for(int j=0;j<=m;j++)dp[0][j]=0;
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s1[i-1]==s2[j-1]){
