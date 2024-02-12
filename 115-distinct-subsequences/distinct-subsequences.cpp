44b7263c228356dbc5a9c06b3e037926b5219dde
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-02-12 19:10:33 +0530

    Time: 9 ms (95.32%) | Memory: 7.6 MB (87.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 private:
     int f(int i,int j,string s1,string s2,vector<vector<int>> &dp){
-        if(j<0)return 1;
-        if(i<0)return 0;
+        if(j==0)return 1;
+        if(i==0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
         if(s1[i]==s2[j]){
             return dp[i][j]=f(i-1,j-1,s1,s2,dp)+f(i-1,j,s1,s2,dp);
@@ -12,7 +12,21 @@ private:
 public:
     int numDistinct(string s, string t) {
         int n = s.size(),m = t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
+        vector<double>prev(m+1,0),cur(m+1,0);
+        
+        prev[0]=cur[0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j = 1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    cur[j]=prev[j-1]+prev[j];
+                }
+                else{
+                    cur[j] = prev[j];
+                }
+            }
+            prev=cur;
+        }
+        return (int)prev[m];
     }
 };
