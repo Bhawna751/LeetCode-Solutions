2be51d65d33364cd6f51bcfe2249af639d2b963e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-02-17 21:21:19 +0530

    Time: 6 ms (74.28%) | Memory: 10.4 MB (23.05%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,63 +1,28 @@
 class Solution {
 public:
-    int mod =100000007;
-    long long f(int i,int j,int isTrue,string &exp){
-        if(i>j)return 0;
-        if(i==j){
-            if(isTrue){
-                return exp[i] == 'T' ? 1:0;
-
-            }
-            else{
-                return exp[i] == 'F' ? 1:0;
-            }
+    bool parseBoolExpr(string expression){
+        auto it = expression.begin();
+        return parse(it);
+    }    
+    bool parse(string :: iterator &it){
+        switch(*(it++)){
+            case 't': return true;
+            case 'f': return false;
+            case '|': return parse(it,false, [](bool init, bool val){return init || val;});
+            case '&': return parse(it,true, [](bool init, bool val){return init && val;});
+            case '!': return parse(it,false, [](bool init, bool val){return !val;}); 
         }
-        long long way=0;
-        for(int ind=i+1;ind<=j-1;ind+=2){
-            long long lt = f(i,ind-1,1,exp);
-            long long lf = f(i,ind-1,0,exp);
-            long long rt = f(ind+1,j,1,exp);
-            long long rf = f(ind+1,j,0,exp);
-            if(exp[ind]=='&'){
-                if(isTrue) way = (way + (rt*lt)%mod)%mod;
-                else way = (way + (rt*lf)%mod+(rf & lt)%mod + (rf & lf) %mod)%mod;
-            }
-            else if(exp[ind] == '|'){
-                if(isTrue){
-                    way = (way + (lt*rt)%mod + (lt*rf)%mod + (lf*rt)%mod) %mod;
-                }
-                else{
-                    way = (way + (rf*lf)%mod)%mod;
-                }
-            }
-            else{
-                if(isTrue){
-                    way = (way+ (lt*rf)%mod + (lf*rt)%mod)%mod;
-                }
-                else{
-                    way = (way + (lt*rt)%mod + (lt*rf)%mod + (lf*rt)%mod) %mod;
-                }
-            }
-        }
-        return way;
+        return false;
     }
-    bool parseBoolExpr(string expression) {
-        stack<char>st;
-        for(auto it : expression){
-            if(it==')'){
-                auto t = false,f = false;
-                while(st.top()=='t' || st.top()=='f'){
-                    t |= st.top() == 't';
-                    f |= st.top() == 'f';
-                    st.pop();
-                }
-                auto op = st.top();
-                st.pop();
-                st.push(op == '!' ? f ? 't' : 'f' : op == '&' ? !f ? 't' : 'f' : t ? 't' : 'f');
-
-            }
-            else if(it != ',' && it != '(')st.push(it);
+    bool parse(string::iterator &it,bool init,function <bool(bool,bool)>op){
+        it++;
+        while(true){
+            auto b = parse(it);
+            init = op(init,b);
+            if (*it == ')') break;
+            it++;
         }
-        return st.top() == 't' ? true : false;
+        it++;
+        return init;
     }
 };
