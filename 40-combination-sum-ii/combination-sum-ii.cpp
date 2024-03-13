b53649be42034045b1f398107d403fe85fcbeff5
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-03-13 09:22:38 +0530

    Time: 8 ms (27.16%) | Memory: 12.6 MB (49.94%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void subset(int ind,vector<int>&candidates,int target,vector<int>&ds,vector<vector<int>>&ans){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<candidates.size();i++){
+            if(i>ind && candidates[i]==candidates[i-1])continue;
+            if(candidates[i]>target)break;
+            ds.push_back(candidates[i]);
+            subset(i+1,candidates,target-candidates[i],ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        subset(0,candidates,target,ds,ans);
+        return ans;
+    }
+};
