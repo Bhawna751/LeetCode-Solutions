b276b30f94f10de86ac0a121b45ddfde51bfbeca
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-03-23 08:38:19 +0530

    Time: 78 ms (76.66%) | Memory: 58.6 MB (57.10%) - LeetSync

--- 1375-find-palindrome-with-fixed-length/find-palindrome-with-fixed-length.cpp
+++ 1375-find-palindrome-with-fixed-length/find-palindrome-with-fixed-length.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    long long palindrome(int j,int intLength,int exponent){
-        long long j1 = pow(10,exponent);
-        long long j2 = j1 + (j-1);
-        string s1 = to_string(j2);
-        string s2 = "";
-        for(int i=1;i<=(intLength/2);i++){
-            s2 += s1[i-1];
+    int image(long long n,int length){
+        long long ans=0;
+        if(length%2==1){
+            n = n/10;
         }
-        reverse(s2.begin(),s2.end());
-        s1 = s1+s2;
-        long long ans = stoll(s1);
+        while(n>0){
+            ans = ans * 10 + n % 10;
+            n = n/10;
+        }
         return ans;
     }
     vector<long long> kthPalindrome(vector<int>& queries, int intLength) {
-        int n = queries.size();
-        vector<long long> palindromes(n,0);
-        int exponent;
-        if(intLength%2 == 0) exponent = intLength/2 - 1;
-        else exponent = intLength/2;
-        int limit = 9 * pow(10,exponent);
-        for(int i=0;i<n;i++){
-            int j = queries[i];
-            if(j>limit) palindromes[i]=-1;
-            else palindromes[i] = palindrome(j,intLength,exponent);
+        vector<long long> ans;
+        long long first = pow(10,(intLength+1)/2 -1);
+        long long last = pow(10,(intLength+1)/2);
+        long long pal = pow(10,intLength/2);
+        for(int it:queries){
+            if(first+it > last){
+                ans.push_back(-1);
+            }
+            else{
+                long long i = (first + it -1) * pal;
+                long long j = image(first + it -1,intLength);
+                ans.push_back(i+j);
+            }
         }
-        return palindromes;
+        return ans;
     }
 };
