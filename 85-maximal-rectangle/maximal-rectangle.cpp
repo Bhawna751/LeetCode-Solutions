de4395078b12a3324c60967f7d3d20e69477d2cf
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-04-13 23:10:57 +0530

    Time: 26 ms (88.34%) | Memory: 16.8 MB (87.72%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix[0].size()+1,m=matrix.size();
+        if(matrix.size()<=0 || matrix[0].size()<=0)return 0;
+        int t=0,w=0,r=0;
+        vector<int> h(n,0);
+        for(int i=0;i<m;i++){
+            stack<int> s;
+            for(int j=0;j<n;j++){
+                if(j<n-1){
+                    if(matrix[i][j]=='1') h[j]+=1;
+                    else h[j]=0;
+                }
+                while(!s.empty() && h[s.top()]>=h[j]){
+                    t=h[s.top()];
+                    s.pop();
+                    w=s.empty() ? j:j-s.top()-1;
+                    if(t*w>r) r=t*w;
+                }
+                s.push(j);
+            }
+        }
+        return r;
+    }
+};
