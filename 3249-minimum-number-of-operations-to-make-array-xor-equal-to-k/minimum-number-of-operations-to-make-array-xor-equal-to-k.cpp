00048ae7619b22791f0ee03a3e12e7fb7c4d0c92
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-04-29 08:57:51 +0530

    Time: 106 ms (71.03%) | Memory: 91.4 MB (54.80%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -5,11 +5,11 @@ public:
         int ans=k;
         for(int i=0;i<n;i++){
             ans = ans^nums[i];
-            cout<<ans<<endl;
+           // cout<<ans<<endl;//3 2 1 5
         }
         int cnt=0;
-        while(ans){
-            cnt += ans & 1;
+        while(ans){//5
+            cnt += ans & 1;//
             ans>>=1;
         }
         return cnt;
