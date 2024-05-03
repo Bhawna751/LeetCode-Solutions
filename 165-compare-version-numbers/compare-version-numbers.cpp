912fd12600d279e71b51e83802ff92b9043b7033
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-03 08:09:47 +0530

    Time: 3 ms (39.03%) | Memory: 7.3 MB (77.28%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        int i=0,j=0,n1=0,n2=0,v1=version1.size(),v2=version2.size();
-        while(i<v1 || j<v2){
-            n1=0,n2=0;
-            while(i<v1 && version1[i]!='.'){
-                n1 = n1*10 + (version1[i]-'0');
+        int n=version1.length(),m=version2.length(),i=0,j=0;
+        while(i<n && j<m){
+            long long n1=0,n2=0;
+            while(i<n && version1[i]!='.'){
+                n1 = n1*10 + version1[i]-'0';
                 i++;
             }
-            i++;
-            while(j<v2 && version2[j]!='.'){
-                n2 = n2*10 + (version2[j]-'0');
+            while(j<m && version2[j]!='.'){
+                n2 = n2*10 + version2[j]-'0';
                 j++;
             }
-            j++;
-            if(n1>n2) return 1;
+            if(n1>n2)return 1;
             else if(n1<n2) return -1;
+            i++;j++;
+        }
+        while(i<n){
+            long long n1 = 0;
+            while(i<n && version1[i]!='.'){
+                n1= n1*10 + version1[i]-'0';
+                i++;
+            }
+            if(n1>0) return 1;
+            i++;
+        }
+        while(j<m){
+            long long n2 = 0;
+            while(j<m && version2[j]!='.'){
+                n2= n2*10 + version2[j]-'0';
+                j++;
+            }
+            if(n2>0) return -1;
+            j++;
         }
         return 0;
     }
