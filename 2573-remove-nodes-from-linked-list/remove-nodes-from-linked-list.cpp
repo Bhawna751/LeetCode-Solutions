9694efa0f97888c1321bae036c28e53f0dacbcb3
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-06 10:09:31 +0530

    Time: 582 ms (5.00%) | Memory: 239 MB (5.47%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -11,12 +11,48 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        if(head==nullptr || head->next == nullptr) return head;
-        ListNode *temp = removeNodes(head->next);
-        if(temp->val > head->val){
-            return temp;
+        vector<int> ind;
+        ListNode *temp=head,*temp1=head;
+        while(temp!=nullptr){
+            ind.push_back(temp->val);
+            temp=temp->next;
         }
-        head->next = temp;
-        return head;
+        temp=head;
+        int n= ind.size();
+        stack<int> st;
+        vector<int> nextGreater;
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && ind[st.top()]<=ind[i]) st.pop();
+            if(st.empty()) nextGreater.push_back(-1);
+            else nextGreater.push_back(st.top());
+            st.push(i);
+        }
+
+        reverse(nextGreater.begin(),nextGreater.end());
+        set<int>s;
+        for(int i=0;i<nextGreater.size();i++){
+            if(nextGreater[i]!=-1) s.insert(i);
+        }
+        int i=0;
+        ListNode *prev = nullptr;
+        while(temp){
+            if(s.find(i)!=s.end()){
+                if(temp==temp1){
+                    temp=temp->next;
+                    temp1=temp1->next;
+                }
+                else{
+                    prev->next = temp->next;
+                    temp=temp->next;
+                }
+            }
+            else{
+                prev= temp;
+                temp=temp->next;
+            }
+            i++;
+        }
+        return temp1;
     }
 };
