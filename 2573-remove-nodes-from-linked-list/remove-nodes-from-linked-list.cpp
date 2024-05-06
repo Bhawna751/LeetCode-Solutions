672f74a80589550fc5ea5c3e5f2ffabc3ae094ea
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-06 09:41:21 +0530

    Time: 258 ms (81.55%) | Memory: 164.5 MB (65.64%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if(head==nullptr || head->next == nullptr) return head;
+        ListNode *temp = removeNodes(head->next);
+        if(temp->val > head->val){
+            return temp;
+        }
+        head->next = temp;
+        return head;
+    }
+};
