e893bed2f1556ea1e740569de1b8bbf692b64bf3
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-16 10:09:53 +0530

    Time: 4 ms (97.31%) | Memory: 17.3 MB (64.84%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -12,30 +12,10 @@
 class Solution {
 public:
     bool evaluateTree(TreeNode* root) {
-        stack<TreeNode*> st;
-        st.push(root);
-        unordered_map<TreeNode*, bool> mpp;
-        while(!st.empty()){
-            TreeNode* top = st.top();
-            if(!top->left && !top->right){
-                st.pop();
-                mpp[top] = top->val;
-                continue;
-            }
-            if(mpp.find(top->left) != mpp.end() && mpp.find(top->right) != mpp.end()){
-                st.pop();
-                if(top->val == 2){
-                    mpp[top] = mpp[top->left] || mpp[top->right];
-                }
-                else{
-                    mpp[top] = mpp[top->left] && mpp[top->right];
-                }
-            }
-            else{
-                st.push(top->right);
-                st.push(top->left);
-            }
-        }
-        return mpp[root] ==1;
+        if(root->val == 1) return true;
+        if(root->val == 0)return false;
+        if(root->val == 2) return evaluateTree(root->left) || evaluateTree(root->right);
+        if(root->val == 3) return evaluateTree(root->left) && evaluateTree(root->right);
+        return true;
     }
 };
