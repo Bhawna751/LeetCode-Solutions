a2516fadb3725edefb7aaa2840ba2ad889caf201
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-22 09:39:15 +0530

    Time: 97 ms (52.03%) | Memory: 61 MB (51.14%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string>path;
-        subset(0,s,ans,path);
-        return ans;
-    }
-    void subset(int ind,string s,vector<vector<string>> &ans,vector<string> &path){
-        if(ind == s.size()){
-            ans.push_back(path);
+    void subsets(int ind,string s,vector<vector<string>> &ans,vector<string>&part){
+        if(ind==s.size()){
+            ans.push_back(part);
             return;
         }
         for(int i=ind;i<s.size();i++){
             if(isPalindrome(s,ind,i)){
-                path.push_back(s.substr(ind,i-ind+1));
-                subset(i+1,s,ans,path);
-                path.pop_back();
+                part.push_back(s.substr(ind,i+1-ind));
+                subsets(i+1,s,ans,part);
+                part.pop_back();
             }
         }
     }
-    bool isPalindrome(string s, int ind, int i){
+    bool isPalindrome(string s,int ind,int i){
         while(ind<=i){
-            if(s[ind++]!=s[i--])return false;
+            if(s[ind]!=s[i]){
+                return false;
+            }
+            ind++;
+            i--;
         }
         return true;
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> part;
+        subsets(0,s,ans,part);
+        return ans;
     }
 };
