5393b9a951b6e4d0e735e893693ba95e69145621
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-23 09:17:44 +0530

    Time: 400 ms (45.46%) | Memory: 35.7 MB (92.77%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int generate(vector<int>& nums,int ind, int k,int mask){
+        int n=nums.size();
+        if(ind==n) return mask>0 ? 1:0;
+        bool isBeautiful=true;
+        for(int i=0;i<ind && isBeautiful;i++){
+            isBeautiful = ((1<<i)&mask) == 0 || abs(nums[i] - nums[ind]) != k;
+        }
+        int notpick = generate(nums,ind+1,k,mask);
+        int pick;
+        if(isBeautiful){
+            pick = generate(nums,ind+1,k,mask+(1<<ind));
+        }
+        else pick =0;
+        return notpick + pick;
+    }
+    int beautifulSubsets(vector<int>& nums, int k) {
+        vector<int>subset;
+        int n=nums.size();
+        int ans=generate(nums,0,k,0);
+        return ans;
+    }
+};
