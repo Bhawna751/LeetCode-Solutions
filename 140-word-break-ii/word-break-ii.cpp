5dd088d5c0e86daf7e6112ad1c729d53909f9718
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-25 12:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.79%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void solve(string &s,unordered_set<string> &wordSet,string &cur,vector<string> &ans,int ind){
-        if(ind==s.length()){
+    void solve(string s,unordered_set<string> &wordSet,vector<string> &temp,vector<string> &ans){
+        if(0==s.length()){
+            string cur="";
+            for(auto it:temp) cur+= it+" ";
+            cur.pop_back();
             ans.push_back(cur);
             return;
         }
-        for(int i=ind+1;i<=s.length();i++){//4-->10
-            string word = s.substr(ind,i-ind);//word = s
-            if(wordSet.find(word) != wordSet.end()){
-                string org = cur;//org = "cat"
-                if(!cur.empty()) cur += " ";
-                cur+= word;//cur = "cats"
-                solve(s,wordSet,cur,ans,i);
-                cur=org;
+        for(int i=0;i<s.length();i++){//4-->10
+            if(wordSet.count(s.substr(0,i+1))){
+                temp.push_back(s.substr(0,i+1));
+                solve(s.substr(i+1),wordSet,temp,ans);
+                temp.pop_back();
             }
         }
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
-        vector<string> ans;
-        string cur;
-        solve(s,wordSet,cur,ans,0);
+        vector<string> ans,temp;
+        solve(s,wordSet,temp,ans);
         return ans;    
     }
 
