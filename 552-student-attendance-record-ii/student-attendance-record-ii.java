ee19a6bce94bc69d06f36e361dad598da33d3533
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-26 16:19:19 +0530

    Time: 785 ms (5.06%) | Memory: 96.4 MB (14.44%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int checkRecord(int n) {
         int mod = 1_000_000_007;
-        int[][][] dp = new int[100001][3][4];
+        long[][][] dp = new long[100001][3][4];
         for(int a=0;a<=1;a++){
             for(int l=0;l<=2;l++){
                 dp[0][a][l]=1;
@@ -10,12 +10,12 @@ class Solution {
         for(int day=1;day<=n;day++){
             for(int a=0;a<=1;a++){
                 for(int l=0;l<=2;l++){
-                    dp[day][a][l] = dp[day-1][a][0]%mod;
+                    dp[day][a][l] = dp[day-1][a][0];
                     dp[day][a][l] = (dp[day][a][l] + dp[day-1][a+1][0])%mod;
-                    dp[day][a][l] = (dp[day][a][l] + dp[day-1][a][l+1]) % mod;
+                    dp[day][a][l] = (dp[day][a][l] + dp[day-1][a][l+1])%mod;
                 }
             }    
         }
-        return dp[n][0][0];
+        return (int)(dp[n][0][0]);
     }
 }
