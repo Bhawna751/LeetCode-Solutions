763f51c93c1dda422766a117728e90c10d795e03
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-26 15:45:03 +0530

    Time: 89 ms (79.04%) | Memory: 17.4 MB (81.99%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -3,21 +3,23 @@
 
 class Solution {
 public:
-    ll dp[100001][3][3];
-    ll solve(int n,int a,int l){
-        if(n==0)return 1;
-        if(dp[n][a][l]!=-1){
-            return dp[n][a][l];
-        }
-        ll ans=0;
-        ans= solve(n-1,a,0) % mod;
-        if(a<1) ans = (ans + solve(n-1,a+1,0)) % mod;
-        if(l<2) ans = (ans + solve(n-1,a,l+1))%mod;
-        return dp[n][a][l]= ans % mod;
-    }
-
+    ll dp[100001][3][4];
     int checkRecord(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n,0,0);
+        memset(dp,0,sizeof(dp));
+        for(int a=0;a<=1;a++){
+            for(int l=0;l<=2;l++){
+                dp[0][a][l]=1;
+            }
+        }
+        for(int day=1;day<=n;day++){
+            for(int a=0;a<=1;a++){
+                for(int l=0;l<=2;l++){
+                    dp[day][a][l] = dp[day-1][a][0]%mod;
+                    dp[day][a][l] = (dp[day][a][l] + dp[day-1][a+1][0])%mod;
+                    dp[day][a][l] = (dp[day][a][l] + dp[day-1][a][l+1]) % mod;
+                }
+            }    
+        }
+        return dp[n][0][0];
     }
 };
