6b5395d400542165f36403a99a4f1641ed4adb34
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-29 11:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (47.06%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    void addOne(string &s){
-        int op=0;
-        int n=s.length()-1;
-        while(n>=0 && s[n]!='0'){ 
-            s[n]='0';
-            n--;            
-        }
-        if(n<0){
-            s = '1' + s;
-        }
-        else{
-            s[n]='1';
-        }
-    }
     int numSteps(string s) {
-        int n= s.size();
+        int n=s.size();
         int op=0;
-        while(s.size()>1){
-            n=s.size();
-            if(s[n-1] == '0') s.pop_back();//divide
-            else addOne(s);
-            op++;
+        int carry=0;
+        for(int i=n-1;i>0;i--){
+            if(((s[i]-'0') + carry) % 2){
+                op += 2;
+                carry = 1;
+            }
+            else op++;
         }
-        return op;
+        return op + carry;
     }
 };
