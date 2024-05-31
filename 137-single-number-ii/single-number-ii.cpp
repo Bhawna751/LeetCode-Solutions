fab68998004ecea7da2546c4967e204c5ab840c3
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-31 10:47:21 +0530

    Time: 10 ms (19.46%) | Memory: 13.5 MB (16.48%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) { //[0,1,0,1,0,1,99]
-        sort(nums.begin(),nums.end());//0 0 0 1 1 1 99
-        int n=nums.size();//7
-        if(n==1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        for(int i = 1;i<n-1;i++){
-            if(nums[i] != nums[i+1] && nums[i]!= nums[i-1]) return nums[i];
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(int i:nums){
+            mpp[i]++;
+        }
+        for(auto it:mpp){
+            if(it.second == 1) return it.first;
         }
         return -1;
     }
