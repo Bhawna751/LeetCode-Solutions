38972ee63e8953c3e9a9fc5bde08b01430befbac
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-05-31 10:45:59 +0530

    Time: 10 ms (19.46%), Space: 11.9 MB (57.67%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {//[0,1,0,1,0,1,99] 
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            bool found=false;
-            for(int j=0;j<n;j++){
-                if(nums[i] == nums[j] && i!=j) found = true;
-            }
-            if(!found) return nums[i];
+    int singleNumber(vector<int>& nums) { //[0,1,0,1,0,1,99]
+        sort(nums.begin(),nums.end());//0 0 0 1 1 1 99
+        int n=nums.size();//7
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        for(int i = 1;i<n-1;i++){
+            if(nums[i] != nums[i+1] && nums[i]!= nums[i-1]) return nums[i];
         }
         return -1;
     }
