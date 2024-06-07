99d0e962f4c903d80c21c8bb0bfe43dd09e1ffd7
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-07 09:41:41 +0530

    Time: 46 ms (88.98%), Space: 30.7 MB (54.6%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -3,16 +3,21 @@ public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n=hand.size();
         if(n % groupSize !=0) return false;
-        map<int,int> mpp;
+        unordered_map<int,int> mpp;
         for(int i=0;i<n;i++){
             mpp[hand[i]]++;
         }
-        while(!mpp.empty()){
-            int cur = mpp.begin()->first;
-            for(int i=0;i<groupSize;i++){
-                if(mpp[cur+i] == 0) return false;
-                mpp[cur+i]--;
-                if(mpp[cur+i]<1) mpp.erase(cur+i);
+        for(int it : hand){
+            int start = it;
+            while(mpp[start-1]) start--;
+            while(start <= it){
+                while(mpp[start]){
+                    for(int next = start; next< start+ groupSize;next++){
+                        if(!mpp[next]) return false;
+                        mpp[next]--;
+                    }
+                }
+                start++;
             }
         }
         return true;
