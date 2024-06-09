7412a56433480be68f56fa220792b44923a6c210
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-09 12:59:39 +0530

    Time: 20 ms (39.92%) | Memory: 34.1 MB (34.51%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -3,12 +3,13 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
         
-        int right = accumulate(nums.begin(),nums.end(),0);
-        int left =0; 
+        int total = accumulate(nums.begin(),nums.end(),0);
+        int prefix = 0;
+        int suffix = total; 
         for(int i=0;i<nums.size();i++){
-            right = right - nums[i];
-            if(right == left) return i;
-            left += nums[i];
+            if(i != 0) prefix += nums[i-1];
+            suffix = total - prefix - nums[i];
+            if(suffix == prefix) return i;
         }
         return -1;
     }
