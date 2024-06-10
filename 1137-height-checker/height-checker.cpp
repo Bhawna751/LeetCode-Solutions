3918fa72626abf73fe0d6843dc3191daedca8d31
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-10 09:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (44.04%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -10,21 +10,12 @@ public:
             }
         }
     }
-    void mergeSort(vector<int> &arr,int left,int mid,int right,vector<int> &temp){
-        int s1 = left,s2 = mid+1;
-        int n1=mid-left+1, n2=right-mid;
-        for(int i=0;i<n1;i++){
-            temp[s1+i] = arr[s1+i];
-        }
-        for(int i=0;i<n2;i++){
-            temp[s2+i] = arr[s2+i];
-        }
-        
-    }
+   
+    
     int heightChecker(vector<int>& heights) {
         int n=heights.size();
         vector<int> expected=heights;
-        bubbleSort(expected);
+       sort(expected.begin(),expected.end());
         int cnt=0;
         for(int i=0;i<n;i++){
             if(heights[i] != expected[i]) cnt++;
