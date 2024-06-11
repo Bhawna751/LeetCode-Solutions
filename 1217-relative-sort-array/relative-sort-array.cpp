7617e5860cf452e07b5268e960242dd574037fff
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-11 07:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (80.92%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> result,remaining;
-        unordered_map<int,int> mpp;
-        for(int i=0;i<arr2.size();i++){
-            mpp[arr2[i]] = 0;
+        int maxi= *max_element(arr1.begin(),arr1.end());//maxi = 19
+        vector<int> freq(maxi+1);//_ 1 3 2 1 _ 1 1 _ 1 ______ 1
+        for(int it:arr1){
+            freq[it]++;
         }
-        for(int i=0;i<arr1.size();i++){
-            if(mpp.find(arr1[i]) != mpp.end()) mpp[arr1[i]]++;
-            else remaining.push_back(arr1[i]);
-        }
-        sort(remaining.begin(),remaining.end());
-        for(int i=0;i<arr2.size();i++){
-            for(int j=0;j<mpp[arr2[i]];j++){
-                result.push_back(arr2[i]);
+        vector<int> result;
+        for(int it:arr2){
+            while(freq[it]>0){
+                result.push_back(it);//result-> 2 2 2 1 4 3 3 9 6 
+                freq[it]--;
             }
         }
-        for (int i = 0; i < remaining.size(); i++) {
-            result.push_back(remaining[i]);
+        for(int i= 0;i<=maxi;i++){
+            while(freq[i]>0){
+                result.push_back(i);
+                freq[i]--;
+            }
         }
         return result;
     }
