6f2b5290eac8ae647b557e3be941696c01c8cf24
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-13 20:15:12 +0530

    Time: 4 ms (74.53%) | Memory: 21.3 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -7,20 +7,20 @@ public:
         }
         return maxi;
     }
-    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        int maxi=max(findmax(seats),findmax(students));
-        vector<int> diff(maxi,0);
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {//3 1 5      2 7 4
+        int maxi=max(findmax(seats),findmax(students));//maxi = max(5,7) = 7
+        vector<int> diff(maxi,0);//0 0 1 0 0 0 0
         for(int it:seats){
-            diff[it-1]++;
+            diff[it-1]++;//
         }
         for(int it:students){
             diff[it-1]--;
         }
         int moves = 0;
-        int unmatched = 0;
+        int empty = 0;
         for(int it:diff){
-            moves += abs(unmatched);
-            unmatched += it;
+            moves += abs(empty);
+            empty += it;
         }
         return moves;
     }
