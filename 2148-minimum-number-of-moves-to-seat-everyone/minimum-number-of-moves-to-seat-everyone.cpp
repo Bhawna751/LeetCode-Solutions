71e0e6dd3bc40091cbb6b95a29679075e1a37fcf
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-13 20:10:38 +0530

    Time: 7 ms (60.18%) | Memory: 21.4 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
+    int findmax(vector<int> arr){
+        int maxi=0;
+        for(int it:arr){
+            if(maxi<it) maxi=it;
+        }
+        return maxi;
+    }
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        int n=students.size();
-        sort(students.begin(),students.end());
-        sort(seats.begin(),seats.end());
-        int moves=0;
-        for(int i=0;i<n;i++){
-            moves += abs(seats[i]-students[i]);
+        int maxi=max(findmax(seats),findmax(students));
+        vector<int> diff(maxi,0);
+        for(int it:seats){
+            diff[it-1]++;
+        }
+        for(int it:students){
+            diff[it-1]--;
+        }
+        int moves = 0;
+        int unmatched = 0;
+        for(int it:diff){
+            moves += abs(unmatched);
+            unmatched += it;
         }
         return moves;
     }
