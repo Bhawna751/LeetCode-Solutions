7f508cce4b6ac6fd8e42a1db56c8e6aceb0e8fff
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-14 09:42:11 +0530

    Time: 82 ms (97.24%), Space: 73.8 MB (15.35%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int minIncrementForUnique(vector<int>& nums) {
-        int moves=0;//2
-        sort(nums.begin(),nums.end());//1 2 3 2 3 7
-        for(int i=1;i<nums.size();i++){//2
-           if(nums[i]==nums[i-1]){//2==2
-                moves++;
-                nums[i]++;
-           }
-           if(nums[i]<nums[i-1]){
-                int dif=nums[i-1]-nums[i]+1;
-                moves+= dif;
-                nums[i] += dif;
-           }
+    int minIncrementForUnique(vector<int>& nums) { // 3 2 1 2 1 7
+        int n=nums.size();// n = 6
+        int maxi=0;// maxi = 7
+        int moves=0;// moves = 6
+        for(int it:nums){
+            maxi = max(maxi,it);
+        }
+        vector<int> freq(maxi+n+1,0);// 0 1 1 1 1 1 0 1 0 0 0 0 0 0 0
+        for(int it:nums){
+            freq[it]++;
+        }
+        for(int i=0;i<freq.size();i++){
+            if(freq[i]<=1)continue;
+            int duplicate = freq[i]-1;// dup=2
+            freq[i+1] += duplicate;//
+            freq[i] = 1;
+            moves += duplicate;
         }
         return moves;
     }
