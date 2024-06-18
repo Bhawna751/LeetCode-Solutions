7d32afd218ed707c2322674de9ba6f24bc0f9a17
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-18 08:58:38 +0530

    Time: 53 ms (51.93%), Space: 41.1 MB (57.35%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        vector<pair<int,int>> p;//{0,0} {2,10} {4,20} {6,30} {8,40} {10,50}
-        p.push_back({0,0});
+        vector<pair<int,int>> p;//{2,10} {4,20} {6,30} {8,40} {10,50}
+        
         for(int i = 0; i<difficulty.size(); i++ ){
-            p.push_back({profit[i],difficulty[i]});
+            p.push_back({difficulty[i],profit[i]});
         }
+        
         sort(p.begin(),p.end());
-        reverse(p.begin(),p.end());
-        for(int i=0;i<p.size()-1;i++){
-            p[i+1].second = min(p[i+1].second,p[i].second);
-        }
-        int total=0;
+        sort(worker.begin(),worker.end());// 4 5 6 7
+
+        int net=0,maxi=0,ind=0;
         for(int i=0;i<worker.size();i++){
-            int job = worker[i];
-            int l=0,r=p.size()-1,profit=0;
-            while(l <= r){
-                int mid = (l+r)/2;
-                if(p[mid].second <= job){
-                    profit = max(p[mid].first, profit);
-                    r = mid - 1;
-                }
-                else{
-                    l = mid + 1;
-                }
+            while(ind<difficulty.size() && worker[i]>= p[ind].first){
+                maxi = max(maxi,p[ind].second);//20
+                ind++;//2
             }
-            total += profit;
+            net += maxi;//20
         }
-        return total;
+        return net;
     }
 };
