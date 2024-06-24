80fb5fe218264c7af1c1adc7f633f8e14f79c995
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-24 13:53:18 +0530

    Time: 86 ms (75.21%) | Memory: 110.5 MB (51.23%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
-        int n = nums.size();//0 1 0  k=1 
-        deque<int> dq;//dq --> 1
-        int flipState = 0, ans = 0;//1
-        for (int i = 0; i < n; i++) {
-            if (i >= k) {
-                flipState = flipState ^ dq.front();
-                dq.pop_front();
+        int n=nums.size();
+        queue<int> q;
+        int flip=0;
+        for(int i =0;i<n;i++){
+            while(!q.empty() && i > q.front() + k - 1){
+                q.pop();
             }
-            if (flipState == nums[i]) {
-                if (i + k > n) return -1;
-                dq.push_back(1);
-                flipState = flipState ^ 1;
-                ans++;
+            if((nums[i] + q.size())%2 == 0){
+                if(i+k>n)return -1;
+                flip++;
+                q.push(i);
             }
-            else dq.push_back(0);
         }
-        return ans;//ans = 1
+        return flip;
     }
 };
