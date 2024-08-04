bb113afe64c9bfdd4d74aa1f935cdfa106ae965a
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-08-04 07:53:37 +0530

    Time: 77 ms (81.35%) | Memory: 11.5 MB (81.35%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int> subarr;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         for(int i=0;i<nums.size();i++){
-            int sum = 0;
-            for(int j=i;j<nums.size();j++){
-                sum += nums[j];
-                subarr.push_back(sum);
+            pq.push({nums[i],i});
+        }
+        int ans=0,mod=1e9+7;
+        for(int i=1;i<=right;i++){
+            auto it = pq.top();
+            pq.pop();
+            if(i>=left) ans = (ans + it.first) % mod;
+            if(it.second < n-1){
+                it.first += nums[++it.second];
+                pq.push(it);
             }
         }
-        sort(subarr.begin(),subarr.end());
-        int sum = 0, mod = 1e9 + 7;
-        for(int i=left-1;i<=right-1;i++){
-            sum = (sum + subarr[i])%mod;
-        }
-        return sum;
+        return ans;
     }
 };
