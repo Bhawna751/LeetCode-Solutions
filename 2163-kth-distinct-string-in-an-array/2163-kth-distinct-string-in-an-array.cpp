6c3852f9d01ff0873ce7b3f781878e70cacb9726
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-08-05 16:49:47 +0530

    Time: 20 ms (36.87%), Space: 18.6 MB (14.46%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_set<string> dis;
-        unordered_set<string> dup;
+        unordered_map<string,int> freq;
+        for(auto it: arr) freq[it]++;
         for(auto it: arr){
-            if(dup.count(it)) continue;
-            if(dis.count(it)){
-                dis.erase(it);
-                dup.insert(it);
-            }else dis.insert(it);
-        }
-        for(auto it: arr){
-            if(!dup.count(it)) k--;
-            if(k == 0) return it;
+            if(freq[it] == 1) k--;
+            if(k==0) return it;
         }
         return "";
     }
