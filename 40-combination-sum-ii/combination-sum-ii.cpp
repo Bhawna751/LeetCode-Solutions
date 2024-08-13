3d49cbc8997e2ee0582c0cdc7925a9a10c9ec27e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-08-13 10:26:16 +0530

    Time: 3 ms (77.81%) | Memory: 12.9 MB (38.57%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void subset(int ind,vector<int>&candidates,int target,vector<int>&ds,vector<vector<int>>&ans){
-        if(target==0){
-            ans.push_back(ds);
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> subset;
+        combination(ans,subset,candidates,target,0);
+        return ans;
+    }
+    void combination(vector<vector<int>>&ans,vector<int>&subset, vector<int>&candidates, int target,int ind){
+        if(target == 0){
+            ans.push_back(subset);
             return;
         }
-        for(int i=ind;i<candidates.size();i++){
-            if(i>ind && candidates[i]==candidates[i-1])continue;
-            if(candidates[i]>target)break;
-            ds.push_back(candidates[i]);
-            subset(i+1,candidates,target-candidates[i],ds,ans);
-            ds.pop_back();
+        for(int i = ind;i<candidates.size() && target>=candidates[i];i++){
+            if(i==ind || candidates[i]!=candidates[i-1]){
+                subset.push_back(candidates[i]);
+                combination(ans,subset,candidates,target-candidates[i],i+1);
+                subset.pop_back();
+            }
         }
-    }
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<vector<int>> ans;
-        vector<int> ds;
-        subset(0,candidates,target,ds,ans);
-        return ans;
     }
 };
