67d001ad9b1ff42a4e3acd2cc85b3bf407efa2dc
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-06 08:51:14 +0530

    Time: 59 ms (67.93%) | Memory: 8.1 MB (96.15%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int lcs(string str1,string str2){
-        int n = str1.size();
-        int m = str2.size();
-        vector<int> prev(m+1,0),cur(m+1,0);
-        for(int j = 0;j<=m;j++)prev[j]=0;
-        for(int i = 0;i<=n;i++)cur[0]=0;
+//TABULATION:
+    // int lcs(string s1,string s2){
+    //     int n=s1.size(), m=s2.size();
+    //     vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    //     for(int i=0;i<=n;i++) dp[i][0]=0;
+    //     for(int i=0;i<=m;i++) dp[0][i]=0;
+    //     for(int i= 1;i<=n;i++){
+    //         for(int j=1;j<=m;j++){
+    //             if(s1[i-1] == s2[j-1]) dp[i][j] = 1+ dp[i-1][j-1];
+    //             else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+    // int longestPalindromeSubseq(string s) {
+    //     string s2 = s;
+    //     reverse(s.begin(),s.end());
+    //     return lcs(s,s2);
+
+    // }
+    int lcs(string s1, string s2){
+        int n=s1.size(), m=s2.size();
+        vector<int> prev(m+1,0), cur(m+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]){
-                    cur[j]=1+prev[j-1];
-                }
-                else cur[j]=max(prev[j],cur[j-1]);
+                if(s1[i-1]==s2[j-1]) cur[j] = 1 + prev[j-1];
+                else cur[j] = max(prev[j], cur[j-1]);
             }
-            prev=cur;
+            prev = cur;
         }
         return prev[m];
     }
     int longestPalindromeSubseq(string s) {
-        string t=s;
-        reverse(t.begin(),t.end());
+        string t = s;
+        reverse(s.begin(),s.end());
         return lcs(s,t);
     }
 };
