86f27b7632bcc32908cd6edc61e588dd8c82e135
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-08 22:17:20 +0530

    Time: 41 ms (12.44%), Space: 21.8 MB (48.97%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int findmax(vector<int> &v){
-        int maxi = INT_MIN;
-        int n = v.size();
+    int findMax(vector<int> &piles){
+        int n = piles.size();
+        int maxi=-1e9;
         for(int i=0;i<n;i++){
-            maxi = max(maxi,v[i]);
+            maxi = max(maxi, piles[i]);
         }
         return maxi;
     }
-    long long calculatetotalhours(vector<int>&v,int hourly){
-        long long totalH =0 ;
-        int n = v.size();
-        for(int i=0;i<n;i++){
-            totalH +=ceil((double)v[i]/(double)hourly);
+    long long calculateTotalHourly(vector<int> &piles, int hourly){
+        int n=piles.size();
+        long long total = 0;
+        for(int i = 0 ; i<n;i++){
+            total += ceil( (double)(piles[i]) / (double)(hourly) );
         }
-        return totalH;
+        return total;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1,high = findmax(piles);
-        while(low<=high){
+        int low = 1, high = findMax(piles);
+        while(low <= high){
             int mid=(low+high)/2;
-            long long totalH = calculatetotalhours(piles,mid);
-            if(totalH<=h){
-                high = mid-1;
-            }
-            else low = mid+1;
+            long long total = calculateTotalHourly(piles,mid);
+            if(total <= h) high = mid - 1;
+            else low = mid + 1;
         }
         return low;
     }
-   
 };
