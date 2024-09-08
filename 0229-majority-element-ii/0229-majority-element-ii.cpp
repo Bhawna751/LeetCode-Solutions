2ff198791fabc492961f7b766c3bb6ffeac2fb5b
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-08 10:07:46 +0530

    Time: 9 ms (57.01%), Space: 18.7 MB (75.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,12 +4,29 @@ public:
         int n = nums.size();
         vector<int> ans;
         int mini = int (n/3) +1;
-        unordered_map<int,int> mpp;
+        int cnt1 = 0, cnt2 = 0, ele1 = -1e9, ele2 = -1e9;
+
         for(int i=0;i<n;i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]] == mini)ans.push_back(nums[i]);
-            if(ans.size() == 2) break;
+            if(cnt1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                cnt1++;
+            }else if(cnt2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                cnt2++;
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else{
+                cnt1--; cnt2--;
+            }
         }
+        cnt1 = 0; cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
+        }
+        if(cnt1 >= mini) ans.push_back(ele1);
+        if(cnt2 >= mini && ele1 != ele2) ans.push_back(ele2);
         return ans;
     }
 };
