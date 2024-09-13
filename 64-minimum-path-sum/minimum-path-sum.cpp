df2be8e56a5b124ecab836aaa2dd547257091d2e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-13 14:55:08 +0530

    Time: 11 ms (33.63%) | Memory: 14.8 MB (22.53%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -11,42 +11,42 @@ public:
     //      return dp[i][j] = min(up,left);
     //  }
 //TABULATION:
-   
+//    int minPathSum(vector<vector<int>>& grid){
+//         int n = grid.size(), m = grid[0].size();
+//         vector<vector<int>> dp (n, vector<int>(m,0));
+//         dp[0][0] = grid[0][0];
+//         for(int i=1;i<m;i++){
+//             dp[0][i] = grid[0][i] + dp[0][i-1];
+//         }
+//         for(int i = 1;i<n;i++) dp[i][0] = grid[i][0] + dp[i-1][0];
+//         for(int i=1;i<n;i++){
+//             for(int j=1;j<m;j++){
+//                 dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+//             }
+//         }
+//         return dp[n-1][m-1];
+//     }
 //Space optimized:
-    // int minPathSum(vector<vector<int>>& grid) {
-    //     int n=grid.size(), m=grid[0].size();
-    //     vector<int> prev(m,0);
-    //     for(int i=0;i<n;i++){
-    //         vector<int> temp(m,0);
-    //         for(int j=0;j<m;j++){
-    //             if(i==0 && j==0) temp[j] = grid[i][j];
-    //             else{
-    //                 int up = grid[i][j];
-    //                 if(i>0) up += prev[j];
-    //                 else up += 1e9;
-    //                 int left = grid[i][j];
-    //                 if(j>0) left += temp[j-1];
-    //                 else left += 1e9;
-    //                 temp[j] = min(up,left);
-    //             }
-    //         }
-    //         prev = temp;
-    //     }
-    //     return prev[m-1];
-    // }
-int minPathSum(vector<vector<int>>& grid){
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp (n, vector<int>(m,0));
-        dp[0][0] = grid[0][0];
-        for(int i=1;i<m;i++){
-            dp[0][i] = grid[0][i] + dp[0][i-1];
-        }
-        for(int i = 1;i<n;i++) dp[i][0] = grid[i][0] + dp[i-1][0];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(), m=grid[0].size();
+        vector<int> prev(m,0);
+        for(int i=0;i<n;i++){
+            vector<int> temp(m,0);
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) temp[j] = grid[i][j];
+                else{
+                    int up = grid[i][j];
+                    if(i>0) up += prev[j];
+                    else up += 1e9;
+                    int left = grid[i][j];
+                    if(j>0) left += temp[j-1];
+                    else left += 1e9;
+                    temp[j] = min(up,left);
+                }
             }
+            prev = temp;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
+
 };
