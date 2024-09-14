09bb91882ae21f0287b343a59696b58365752767
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-14 17:56:12 +0530

    Time: 18 ms (27.68%) | Memory: 16.2 MB (41.17%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool solve(string s, unordered_set<string> &st, int ind, unordered_map<int,bool> &dp){
-        if(ind == s.size()) return true; //successfully segmented
-        if(dp.find(ind) != dp.end()) return dp[ind];
-        for(int i= ind+1;i<=s.size();i++){
-            if(st.find(s.substr(ind,i-ind)) != st.end() && solve(s,st,i,dp)){
-                return dp[ind]=true;
-            }
-        }
-        return dp[ind]= false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string>st(wordDict.begin(), wordDict.end());//"leet" "code"
-        unordered_map<int,bool>dp;
-        return solve(s, st, 0,dp);
+        vector<bool>dp(s.length()+1,false);
+        dp[0]=true; // An empty string can always be segmented
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && st.find(s.substr(j,i-j)) != st.end()){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
     }
 };
