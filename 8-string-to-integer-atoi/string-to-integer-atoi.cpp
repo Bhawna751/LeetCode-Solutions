e1b626b0c983817fc2aff996473757955745f49e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-09-15 12:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (68.81%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,26 +1,16 @@
-typedef long long ll;
 class Solution {
 public:
-    ll solve(string s,ll ans,int sign,int i){
-        if(sign*ans>=INT_MAX)return INT_MAX;
-        else if(sign*ans<=INT_MIN)return INT_MIN;
-        else if(s[i]==' '|| !isdigit(s[i])) return ans*sign;
-        int res=s[i]-'0';
-        return solve(s,ans*10+res,sign,i+1);
-    }
     int myAtoi(string s) {
-        int flag=0,i=0,n=s.size(),sign=1;
-        while(i<n && s[i]==' ')i++;
-        if(s[i]=='-'){
-            sign= -1;
-            i++;
-            flag++;
+        int sign = 1, base = 0, i=0;
+        while(s[i] == ' ') i++;//increment for leading whitespaces
+        if(s[i]=='-' || s[i]=='+') sign = 1-2*(s[i++]=='-');//check for sign and assign flag value accordingly
+        while(s[i] >= '0' && s[i] <= '9'){//till it's a digit b/w 0-9...
+            if(base > INT_MAX/10 || (base == INT_MAX/10 && s[i] - '0' > 7)){//check the overflow condition
+                if(sign == 1) return INT_MAX;
+                else return INT_MIN;
+            }
+            base = 10 * base + (s[i++]-'0');// calculate base value for each digit encountered
         }
-        if(s[i]=='+'){
-            sign=1;
-            i++;flag++;
-        }
-        if(flag>1)return 0;
-        return solve(s,0,sign,i);
+        return base * sign;
     }
 };
