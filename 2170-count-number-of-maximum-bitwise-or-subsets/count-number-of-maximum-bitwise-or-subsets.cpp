9c6c4aa6f844a849fece8c3833500989a88e88cd
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-10-18 18:34:08 +0530

    Time: 127 ms (11.99%) | Memory: 309.1 MB (9.56%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int cntSubsets(vector<int> &nums, int ind, int cur, int maxi){
+    int cntSubsets(vector<int> &nums, int ind, int cur, int maxi,vector<vector<int>> &dp){
         if(ind == nums.size()) return (cur == maxi) ? 1:0;
-        int notpick = cntSubsets(nums,ind+1, cur, maxi);
-        int pick = cntSubsets(nums,ind+1, cur|nums[ind], maxi);
-        return pick+notpick;
+        int notpick = cntSubsets(nums,ind+1, cur, maxi,dp);
+        int pick = cntSubsets(nums,ind+1, cur|nums[ind], maxi,dp);
+        return dp[ind][cur]=pick+notpick;
     }
     int countMaxOrSubsets(vector<int>& nums) {
-        int maxi = 0;
+        int maxi = 0, n= nums.size();
         for(int it:nums){
             maxi = max(maxi, it|maxi);
         }
-        return cntSubsets(nums,0, 0, maxi);
+        vector<vector<int>> dp(n, vector<int> (maxi+1));
+        return cntSubsets(nums,0, 0, maxi,dp);
     }
 };
