c3e67cdf637ed5cea02b8b2f943696878fe81742
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-10-19 13:18:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (43.02%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -3,18 +3,32 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();//[3,2,3,2,3,3]
         vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(ans.size() == 0 || ans[0] != nums[i]){
-                int cnt  =0;
-                for(int j=0;j<n;j++){
-                    if(nums[i] == nums[j]) cnt++;
-                }
-                if(cnt > (n/3))ans.push_back(nums[i]);
+        int mini = n/3 + 1;
+        int cnt1=0,cnt2=0, ele1=-1e9, ele2=-1e9;
 
+        for(int it: nums){
+            if(cnt1 == 0 && ele2 != it){
+                cnt1 = 1;
+                ele1 = it;
             }
-            if(ans.size()==2) break;
-
+            else if(cnt2 == 0 && ele1 != it){
+                cnt2 = 1;
+                ele2 = it;
+            }
+            else if(ele1 == it) cnt1++;
+            else if(ele2 == it) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }   
+        cnt1 = 0, cnt2=0;
+        for(int it:nums){
+            if(it == ele1) cnt1++;
+            if(it == ele2) cnt2++;
+        }
+        if(cnt1 >= mini)ans.push_back(ele1);
+        if(cnt2 >= mini && ele1!=ele2) ans.push_back(ele2);
         return ans; 
     }
 };
