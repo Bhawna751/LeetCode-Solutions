583b7226aa0b87a6c49e41f37e950065fe12eb87
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-10-20 10:45:53 +0530

    Time: 3 ms (94.42%) | Memory: 9.7 MB (64.32%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    bool parseBoolExpr(string expression){
-        auto it = expression.begin();
-        return parse(it);
-    }    
-    bool parse(string :: iterator &it){
-        switch(*(it++)){
-            case 't': return true;
-            case 'f': return false;
-            case '|': return parse(it,false, [](bool init, bool val){return init || val;});
-            case '&': return parse(it,true, [](bool init, bool val){return init && val;});
-            case '!': return parse(it,false, [](bool init, bool val){return !val;}); 
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
+        for(char it: expression){
+            if(it == ',' || it == '(') continue;
+            if(it == 't' || it == 'f' || it == '&' || it == '|' || it == '!') st.push(it);
+            else if(it == ')'){
+                bool t=false,f=false;
+                while(st.top()!='!' && st.top()!='&' && st.top()!='|'){
+                    char top = st.top();
+                    st.pop();
+                    if(top == 't') t= true;
+                    if(top == 'f') f= true;
+                }
+                char op = st.top();
+                st.pop();
+                if(op=='!')st.push(t ? 'f' : 't');
+                else if(op == '&')st.push(f ? 'f' : 't');
+                else st.push(t ? 't' : 'f');
+            }
         }
-        return false;
-    }
-    bool parse(string::iterator &it,bool init,function <bool(bool,bool)>op){
-        it++;
-        while(true){
-            auto b = parse(it);
-            init = op(init,b);
-            if (*it == ')') break;
-            it++;
-        }
-        it++;
-        return init;
+        return st.top() == 't';
     }
 };
