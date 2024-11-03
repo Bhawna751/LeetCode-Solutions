2f092a52084bdf57743f18876eaacd9c4684a04d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-11-03 21:12:05 +0530

    Time: 22 ms (61.96%) | Memory: 69.3 MB (38.99%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    bool possible(vector<int>& bloomDay,int d, int m,int k){
-        int cnt =0, bouq=0;
-        for(int i=0;i<bloomDay.size();i++){
-            if(bloomDay[i]<= d) cnt++;
+    bool possible(int day, vector<int> &bloomDay, int m ,int k){
+        int n = bloomDay.size();
+        int cnt = 0, b=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i] <= day) cnt++;
             else{
-                bouq += cnt/k;
+                b += cnt/k;
                 cnt=0;
             }
         }
-        bouq += cnt/k;
-        return bouq >= m;
+        b += cnt/k;
+        return b>=m;
     }
-    int minDays(vector<int>& bloomDay, int mc, int kc) {
-        long long m = mc, k = kc;
+    int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
-        if(m*k > n)return -1;
-        int mini=1e9,maxi=-1e9;
+        long long bound  = m*1ll*k*1ll;
+        if(bound > n)return -1;
+        int mini = 1e9, maxi = -1e9;
         for(int i=0;i<n;i++){
-            mini  = min(mini,bloomDay[i]);
-            maxi = max(maxi,bloomDay[i]);
+            mini = min(mini, bloomDay[i]);
+            maxi = max(maxi, bloomDay[i]);
         }
-        int low = mini, high = maxi;
+        int ans=-1;
+        int low = mini, high=maxi;
         while(low<=high){
-            int mid = (low+high)/2;
-            if(possible(bloomDay,mid,m,k))high=mid-1;
-            else low=mid+1;
+            int day = (low+high)/2;
+            if(possible(day, bloomDay, m, k)){
+                ans = day;
+                high = day-1;
+            }
+            else low = day+1;
         }
-        return low;
+        return ans;
     }
 };
