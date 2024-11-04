c6bf0283b803f75c4a886ac8a94a857dd2dfbc6d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-11-04 17:27:39 +0530

    Time: 0 ms (100%), Space: 22.9 MB (74.03%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int vis1[9][9] = {0},vis2[9][9] = {0},vis3[9][9] = {0};
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j] != '.'){
+                    int it = board[i][j] - '0' -1;
+                    int k = i/3 * 3+j /3;
+                    if(vis1[i][it] || vis2[j][it] || vis3[k][it]) return false;
+                    vis1[i][it] = 1;
+                    vis2[j][it] = 1;
+                    vis3[k][it] = 1;
+                }
+            }
+        }
+        return true;
+    }
+};
