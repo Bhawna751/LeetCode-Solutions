37d17a0d0002d93d2339de9daabf3b3dff5949eb
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-11-13 15:44:17 +0530

    Time: 55 ms (75.60%) | Memory: 60.4 MB (18.39%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int lower_bound(vector<int>& nums, int low, int high, int ele){
         while(low<=high){
-            int mid = (high+low)/2;
-            if(nums[mid] >= ele) high = mid-1;
-            else low = mid+1;
+            int mid = (high+low)/2;//mid=
+            if(nums[mid] >= ele) high = mid-1;//high  = 1
+            else low = mid+1;//low = 2
             
         }
         return low;
     }
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());//0,1,4,4,5,7 lower  = 3 upper = 6
         long long ans = 0;
         for(int i=0;i<nums.size();i++){
-            int low = lower_bound(nums,i+1,nums.size()-1, lower - nums[i]);
-            int high = lower_bound(nums,i+1, nums.size()-1, upper-nums[i]+1);
-            ans += (high-low)*1LL;
+            int low = lower_bound(nums,i+1,nums.size()-1, lower - nums[i]);//low=2, low=2, high=5, ele = 2
+            int high = lower_bound(nums,i+1, nums.size()-1, upper-nums[i]+1);//high=5, low=1, high=5, ele = 7
+            ans += (high-low)*1LL;//ans = 3
         }
         return ans;
     }
