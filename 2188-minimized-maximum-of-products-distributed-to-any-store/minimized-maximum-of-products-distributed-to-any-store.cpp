168cf3283dfeebacbee5cd4edd25dfab473d5892
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-11-14 19:05:49 +0530

    Time: 112 ms (27.34%) | Memory: 87.3 MB (34.13%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool check(int x, vector<int>&quantities, int n){
         int j=0;
-        int rem=quantities[j];
+        int rem=quantities[0];//rem = 
         for(int i=0;i<n;i++){
             if(rem<=x){
                 j++;
@@ -15,9 +15,9 @@ public:
         return false;
     }
     int minimizedMaximum(int n, vector<int>& quantities) {
-        int l=1, r = *max_element(quantities.begin(), quantities.end());
+        int l=1, r = *max_element(quantities.begin(), quantities.end());//l=1, r=11
         while(l<r){
-            int mid = (l+r)/2;
+            int mid = (l+r)/2;//mid = 6
             if(check(mid,quantities,n))r=mid;
             else l=mid+1;
         }
