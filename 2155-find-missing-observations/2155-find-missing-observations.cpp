5bf83c2d5bae17dcc11b86dc5359a5495b2f8995
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-12-01 11:14:12 +0530

    Time: 12 ms (16.26%), Space: 119.9 MB (27.06%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        
+        int m = rolls.size();
+        int total = n+m, sum =0;
+        for(int i=0;i<m;i++){
+            sum += rolls[i];
+        }
+        int rem = mean * total - sum;
+        int x = rem / n;
+        vector<int> ans(n,x);
+        if(rem > 6*n || rem<n) return {};
+        int mod = rem % n;
+        for(int i =0;i<mod;i++) ans[i]++;
+        return ans;
     }
 };
