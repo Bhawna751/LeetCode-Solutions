7556868566e2befebc943ba93657f6a2fe238e9b
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-01-25 21:34:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (58.72%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<string>&board, vector<vector<string>>&ans,int j){
+    void solve(vector<vector<string>>&ans, vector<string>&board, int j){
         if(j == board.size()){
             ans.push_back(board);
             return;
@@ -8,37 +8,37 @@ public:
         for(int i=0;i<board.size();i++){
             if(valid(board,i,j)){
                 board[i][j] = 'Q';
-                solve(board,ans,j+1);
-                board[i][j] = '.';
+                solve(ans,board,j+1);
+                board[i][j]='.';
             }
         }
     }
-    bool valid(vector<string>&board, int i, int j){
-        int r = i, c=j;
+    bool valid(vector<string>&board, int row, int col){
+        int r=row, c=col;
         while(r>=0 && c>=0){
-            if(board[r][c] == 'Q')return false;
+            if(board[r][c]=='Q')return false;
             r--;
             c--;
         }
-        r=i;
-        c=j;
+        r=row;
+        c=col;
         while(c>=0){
-            if(board[r][c] == 'Q')return false;
+            if(board[r][c]=='Q')return false;
             c--;
         }
-        r=i;
-        c=j;
+        r=row;
+        c=col;
         while(c>=0 && r<board.size()){
-            if(board[r][c] == 'Q')return false;
+            if(board[r][c]=='Q')return false;
             r++;
             c--;
         }
         return true;
     }
-    vector<vector<string> > solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n,string(n,'.'));
-        solve(board,ans,0);
+        solve(ans,board,0);
         return ans;
     }
 };
