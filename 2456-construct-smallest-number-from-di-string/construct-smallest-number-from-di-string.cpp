3335b2c5a1dcebd23e94869b877ecc8161aa0084
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-02-18 11:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (18.66%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    bool match(string &next, string &pattern){
-        for(int i=0;i<pattern.size();i++){
-            if(pattern[i] == 'I'){
-                if(next[i] > next[i+1]) return false;
-            }
-            else{
-                if(next[i] < next[i+1]) return false;
-            }
-        }
-        return true;
-    }
-
     string smallestNumber(string pattern) {
-        int n=pattern.length();
-        string ans="";
-        for(int i=1;i<=n+1;i++){
-            ans.push_back(i+'0');
+        string ans;
+        stack<char>st;
+        for(int i =0;i<= pattern.size();i++){
+            st.push(i+1);
+            if(i == pattern.size() || pattern[i] == 'I'){
+                while(!st.empty()){
+                    ans+= to_string(st.top());
+                    st.pop();
+                }
+            }
         }
-        string next = ans;
-        while(!match(next,pattern)){
-            next_permutation(next.begin(),next.end());
-        }
-        return next;
+        return ans;
     }
 };
