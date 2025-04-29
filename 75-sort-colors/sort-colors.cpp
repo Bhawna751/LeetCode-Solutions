aac22e2a69b91aa452714de4dcfb182676de50dc
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-04-29 23:10:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.11%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int cnt0=0, cnt1=0, cnt2=0;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == 0) cnt0++;
-            else if(nums[i] == 1) cnt1++;
-            else cnt2++;
+        int n=nums.size();
+        int low = 0, mid = 0, high = n-1;
+        //dutch national flag algorithm
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[high], nums[mid]);
+                high--;
+            }
         }
-        //cout<<cnt0<<" "<<cnt1<<" "<<cnt2<<" ";
-        for(int i=0 ; i<cnt0 ; i++)nums[i] = 0;
-        for(int i=cnt0 ; i<cnt1+cnt0 ; i++)nums[i] = 1;
-        for(int i=cnt1+cnt0 ; i<nums.size() ; i++)nums[i] = 2;
-    }
+    }    
 };
