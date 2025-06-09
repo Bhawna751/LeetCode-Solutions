06a5d4dbf6d8cf83668882271752085c68ae6592
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-06-09 09:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (9.23%) - LeetSync

--- 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
+++ 440-k-th-smallest-in-lexicographical-order/k-th-smallest-in-lexicographical-order.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    int count (int n, long pre1, long pre2){
+        int step = 0;
+        while(pre1<=n){
+            step += min((long)(n+1), pre2) -pre1;
+            pre1 *= 10;
+            pre2 *= 10;
+        }
+        return step;
+    }
     int findKthNumber(int n, int k) {
         int cur = 1;
         k--;
         while(k>0){
-            int step = count(n,cur,cur+1);
+            int step = count (n,cur,cur+1);
             if(step <= k){
                 cur++;
-                k -= step;
-            }else{
-                cur *= 10;
+                k = k-step;
+            }
+            else{
+                cur =cur*10;
                 k--;
             }
         }
         return cur;
-    }
-    int count(int n, long prefix1, long prefix2){
-        int steps = 0;
-        while(prefix1 <= n){
-            steps += min((long)(n+1), prefix2) - prefix1;
-            prefix1 *= 10;
-            prefix2 *= 10;
-
-        }
-        return steps;
     }
 };
