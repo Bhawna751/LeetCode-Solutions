04c3d5ed0f99c01f98635b2ef5dcb9c52ecf3b7e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-07-11 21:37:37 +0530

    Time: 0 ms (100.00%) | Memory: 121.1 MB (85.72%) - LeetSync

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        const int n=startTime.size();
+        int busy=0;
+        for (int i=0; i < k; i++) 
+            busy+=endTime[i]-startTime[i];
+
+        if (n==k) return eventTime-busy;
+
+        int ans=startTime[k]-busy;//1st window
+
+        // sliding window
+        for (int l=0, r=k; r<n; l++, r++) {
+            busy+=(endTime[r]-startTime[r])-(endTime[l]-startTime[l]);
+            int end=(r==n-1)?eventTime:startTime[r+1];
+            int start=endTime[l];
+            ans=max(ans, end-start-busy);
+        }
+        return ans;
+    }
+};
+
