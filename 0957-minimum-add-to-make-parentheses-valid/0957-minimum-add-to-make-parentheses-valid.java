5b8a454221e9489f3dbe968ffaf31cac7e233072
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-07-17 10:33:13 +0530

    Time: 1 ms (54.89%), Space: 42 MB (8.48%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int n = s.length();
+        int left=0,right =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')right++;
+            else if(right>0) right--;
+            else left++;
+        }
+        return left+right;
+    }
+}
