af0916e70ba331d3984df4932d2fa00654d337b2
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-01 15:37:10 +0530

    Time: 3 ms (7.69%), Space: 9.5 MB (88.29%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> x(numRows,{1});
-        int i,j;
-        for(i=1;i<numRows;i++){
-            for(j=1;j<i;j++){
-                x[i].push_back(x[i-1][j-1]+x[i-1][j]);
-            }
-            x[i].push_back(1);
+    vector<int> pattern(int n){
+        int sum=1;
+        vector<int> row;
+        row.push_back(1);
+        for(int i=1;i<n;i++){
+            sum = sum * (n-i);
+            sum = sum / i;
+            row.push_back(sum);
         }
-        return x;
+        return row;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for(int i=1;i<=numRows;i++){
+            ans.push_back(pattern(i));
+        }
+        return ans;
     }
 };
