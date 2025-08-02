db315ad36d471ca4ebbab5784ab08a8ddc20fea5
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-02 14:16:42 +0530

    Time: 5 ms (58.94%) | Memory: 19.5 MB (58.50%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& prerequisites) {
-        vector<int> adj[V];
-        for (auto it : prerequisites) {
-            adj[it[0]].push_back(it[1]);
-        }
-        vector<int> indegree(V, 0);
-        queue<int> q;
-        for (int i = 0; i < V; i++) {
-            for (auto it : adj[i]) {
-                indegree[it]++;
+    bool cycle(vector<int> adj[], vector<int> &vis, int node){
+        if(vis[node]==1)return true;
+        if(vis[node]==0){
+            vis[node]=1;
+            for(auto it: adj[node]){
+                if(cycle(adj, vis, it)) return true;
             }
         }
-        vector<int> ans;
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0)
-                    q.push(it);
-            }
-        }
-        if(ans.size()==V)return true;
+        vis[node]=2;
         return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> vis(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(cycle(adj, vis, i)) return false;
+        }
+        return true;
     }
 };
