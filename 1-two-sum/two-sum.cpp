fa224440b551aebf6d3c7b83c93a2c5c3bcc6738
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-13 23:33:19 +0530

    Time: 4 ms (57.18%) | Memory: 17.7 MB (6.31%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -7,19 +7,19 @@ public:
         for(int i=0;i<n;i++){
             ind.push_back({nums[i],i});
         }
-        sort(ind.begin(), ind.end(), [](const vector<int> &a, const vector<int> &b){
+        sort(ind.begin(), ind.end(), [](const vector<int>&a, const vector<int>&b){
             return a[0] < b[0];
         });
-        int left= 0, right = n-1;
-        while(left<right){
-            int sum = ind[left][0] + ind[right][0];
+        int l=0, r=n-1;
+        while(l<r){
+            int sum = ind[l][0] + ind[r][0];
             if(sum == target){
-                ans.push_back(ind[left][1]);
-                ans.push_back(ind[right][1]);
+                ans.push_back(ind[l][1]);
+                ans.push_back(ind[r][1]);
                 return ans;
             }
-            else if(sum<target) left++;
-            else right--;
+            else if(target > sum) l++;
+            else r--;
         }
         return {-1,-1};
     }
