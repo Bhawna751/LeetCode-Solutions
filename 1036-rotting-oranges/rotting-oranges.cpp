326f67a8844b35cc73a048dea9558c4f148e63a1
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-15 11:02:32 +0530

    Time: 3 ms (28.72%) | Memory: 17.2 MB (19.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-        queue<pair<int,int>> coordinates;
-        int total=0,cnt=0,time=0;
+     int n=grid.size(), m=grid[0].size();
+     vector<vector<int>> vis(n,vector<int>(m,-1));
+        int delrow[4] = {0,1,0,-1};
+        int delcol[4] = {1,0,-1,0};
+        int total = 0, cnt =0, time=0;
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] != 0) total++;
-                if(grid[i][j] == 2)coordinates.push({i,j});
+                if(grid[i][j] == 2) q.push({i,j});
+                if(grid[i][j] == 1 || grid[i][j] == 2) total++;
             }
         }
-        int dx[4] = {-1,0,1,0};
-        int dy[4] = {0,-1,0,1};
-        while(!coordinates.empty()){
-            int k = coordinates.size();
+        while(!q.empty()){
+            int k = q.size();
             cnt += k;
             while(k--){
-                int x=coordinates.front().first;
-                int y=coordinates.front().second;
-                coordinates.pop();
-                for(int dir = 0;dir<4;dir++){
-                    int nx = x + dx[dir];
-                    int ny = y + dy[dir];
-                    if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != 1) continue;
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                for(int d = 0;d<4;d++){
+                    int nx = delrow[d] + x;
+                    int ny = delcol[d] + y;
+                    if(nx < 0 || nx >= n || ny < 0 || ny >=m  || grid[nx][ny]!=1) continue;
                     grid[nx][ny] = 2;
-                    coordinates.push({nx,ny});
+                    q.push({nx,ny});
                 }
             }
-            if(!coordinates.empty()) time++;
-        }
-        return total == cnt ? time : -1;
+            if(!q.empty()) time++;
+         }
+         return total == cnt ?  time : -1;
     }
 };
