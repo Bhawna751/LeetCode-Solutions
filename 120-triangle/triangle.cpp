0b22739c0248c8f5b55109ca2a4fb325c6ff1783
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-18 10:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (42.03%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<int> front(n,0),cur(n,0);
-        for(int j=0;j<n;j++) front[j] = triangle[n-1][j];
+    int solve(vector<vector<int>> &triangle, int n,vector<vector<int>>&dp){
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                int d = triangle[i][j] + front[j];
-                int dg = triangle[i][j] + front[j+1];
-                cur[j] = min(d,dg);
+                int down  = triangle[i][j] + dp[i+1][j];
+                int diagonal  = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j]=min(down , diagonal);
             }
-            front=cur;
-        }  
-        return front[0];
+        }
+        return dp[0][0];
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n  = triangle.size();
+        vector<vector<int>> dp(n, vector<int> (n,-1));
+
+        return solve(triangle,n,dp);
     }
 };
