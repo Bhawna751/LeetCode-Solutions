6c3b074ba05729081b03d838aba165d6e66eb2ab
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-18 10:48:27 +0530

    Time: 6 ms (10.39%) | Memory: 15.1 MB (38.92%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&matrix,vector<vector<int>> &dp, int i, int j, int m){
+        if(j>=m || j<0)return 1e9;
+        if(i==0) return matrix[i][j];
+        if(dp[i][j]!=-1)return dp[i][j];
+        int up = matrix[i][j] + solve(matrix,dp, i-1, j, m);
+        int left = matrix[i][j] + solve(matrix,dp, i-1, j-1, m);
+        int right = matrix[i][j] + solve(matrix,dp, i-1, j+1, m);
+        return dp[i][j]=min(up, min(left, right));
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<int> prev(n,0),cur(n,0);
-        for(int j=0;j<n;j++) prev[j] = matrix[0][j];
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n;j++){
-                int u = matrix[i][j] + prev[j];
-                int ld = matrix[i][j];
-                if(j-1>=0) ld += prev[j-1];
-                else ld += 1e8;
-                int rd = matrix[i][j];
-                if(j+1<n) rd+=prev[j+1];
-                else rd+=1e8;
-                cur[j] = min(u,min(ld,rd));
-            }
-            prev=cur;
-        }  
-        int mini =1e8;
-        for(int j=0;j<n;j++){
-            mini = min(mini,prev[j]);
-
+        int m = matrix[0].size();
+        int ans= 1e9;
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        for(int j=0;j<m;j++){
+           dp[0][j] = matrix[0][j];
         }
-        return mini;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int up = matrix[i][j]+dp[i-1][j];
+                int left = matrix[i][j];
+                if(j-1 >= 0)left += dp[i-1][j-1];
+                else left += 1e9;
+                int right = matrix[i][j];
+                if(j+1 < m) right += dp[i-1][j+1];
+                else right += 1e9;
+                dp[i][j] = min(up,min(left, right));
+            }
+        }
+        for(int j=0;j<m;j++){
+            ans = min(ans, dp[n-1][j]);
+        }
+        return ans;
     }
 };
