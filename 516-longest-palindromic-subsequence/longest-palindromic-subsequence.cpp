c3068070dd5d6088b2d1a42d43b4b0da084f944d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-20 23:13:22 +0530

    Time: 95 ms (22.40%) | Memory: 75.8 MB (74.04%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,40 +1,26 @@
 class Solution {
 public:
-//TABULATION:
-    // int lcs(string s1,string s2){
-    //     int n=s1.size(), m=s2.size();
-    //     vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-    //     for(int i=0;i<=n;i++) dp[i][0]=0;
-    //     for(int i=0;i<=m;i++) dp[0][i]=0;
-    //     for(int i= 1;i<=n;i++){
-    //         for(int j=1;j<=m;j++){
-    //             if(s1[i-1] == s2[j-1]) dp[i][j] = 1+ dp[i-1][j-1];
-    //             else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-    //         }
-    //     }
-    //     return dp[n][m];
-    // }
-    // int longestPalindromeSubseq(string s) {
-    //     string s2 = s;
-    //     reverse(s.begin(),s.end());
-    //     return lcs(s,s2);
-
-    // }
-    int lcs(string s1, string s2){
-        int n=s1.size(), m=s2.size();
-        vector<int> prev(m+1,0), cur(m+1,0);
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]) cur[j] = 1 + prev[j-1];
-                else cur[j] = max(prev[j], cur[j-1]);
-            }
-            prev = cur;
+    int solve(string &s, string &t){
+        int n=s.size(), m=t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
-        return prev[m];
+        for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int ind1 = 1; ind1 <= n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s[ind1-1] == t[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+            }
+        }
+        return dp[n][m];
     }
     int longestPalindromeSubseq(string s) {
+        int n = s.length();
         string t = s;
-        reverse(s.begin(),s.end());
-        return lcs(s,t);
+        reverse(t.begin(), t.end());
+        return solve(s,t);
     }
 };
