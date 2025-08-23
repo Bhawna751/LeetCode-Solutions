32480cc5767f22fad963411e522b9bdb69b25d20
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-23 09:20:02 +0530

    Time: 35 ms (49.17%) | Memory: 31.5 MB (37.97%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int lcs(string s1,string s2){
-        int n = s1.size();
-        int m=s2.size();
-        vector<int>prev(m+1,0),cur(m+1,0);
-        
-        for(int i=1;i<=n;i++){
-            for(int j =1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    cur[j]=1+prev[j-1];
-                }
-                else cur[j]=max(prev[j],cur[j-1]);
+    int solve(string s, string t){
+        int n=s.size(), m= t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        for(int j=0;j<=m;j++) dp[0][j] = 0;
+        for(int ind1 = 1;ind1<=n;ind1++){
+            for(int ind2 = 1;ind2<=m;ind2++){
+                if(s[ind1-1] == t[ind2-1]) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
             }
-            prev=cur;
         }
-        return prev[m];
-    }
-    int longestPalin(string s){
-        string t=s;
-        reverse(t.begin(),t.end());
-        return lcs(s,t);
+        return dp[n][m];
     }
     int minInsertions(string s) {
-        return s.size()-longestPalin(s);
+        string t =s;
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        int lcs = solve(s,t);
+        return n-lcs;
     }
 };
