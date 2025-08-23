0803d9bf20718f1cd250befb5db57b8474e92f0e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-23 09:40:34 +0530

    Time: 13 ms (44.21%) | Memory: 17.1 MB (17.72%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int lcs(string s1,string s2){
-        int n = s1.size();
-        int m = s2.size();
-        vector<int> prev(m+1,0),cur(m+1,0);
-        for(int j =0;j<=m;j++) prev[j]=0;
-        for(int i =0;i<=n;i++) prev[0]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1])cur[j]=1+prev[j-1];
-                else cur[j]=max(prev[j],cur[j-1]);
+    int lcs(string word1, string word2){
+        int n =word1.size(), m=word2.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        for(int j=0;j<=m;j++) dp[0][j] = 0;
+        for(int ind1=1;ind1<=n;ind1++) {
+            for(int ind2 = 1;ind2 <= m; ind2++){
+                if(word1[ind1-1] == word2[ind2-1]) dp[ind1][ind2] = 1 +dp[ind1-1][ind2-1];
+                else dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
             }
-            prev=cur;
         }
-        return prev[m];
+        return dp[n][m];
     }
     int minDistance(string word1, string word2) {
-        return word1.size()+word2.size()-2*lcs(word1,word2);
+        int n = word1.size(), m=word2.size();
+        int len = lcs(word1, word2);
+        return (n-len) + (m-len);
     }
 };
