db014451fb92e0f099f982cd1731e49be89ce25f
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-08-25 11:58:26 +0530

    Time: 35 ms (34.84%) | Memory: 27 MB (62.89%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,25 @@
 class Solution {
-private:
-    int f(int i,int j,string s1,string s2,vector<vector<int>> &dp){
-        if(j==0)return 1;
-        if(i==0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j]){
-            return dp[i][j]=f(i-1,j-1,s1,s2,dp)+f(i-1,j,s1,s2,dp);
-        }
-        return dp[i][j]=f(i-1,j,s1,s2,dp);
-    }
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size(),m = t.size();
-        vector<double>prev(m+1,0),cur(m+1,0);
-        
-        prev[0]=cur[0]=1;
-        
-        for(int i=1;i<=n;i++){
-            for(int j = 1;j<=m;j++){
-                if(s[i-1]==t[j-1]){
-                    cur[j]=prev[j-1]+prev[j];
-                }
-                else{
-                    cur[j] = prev[j];
-                }
-            }
-            prev=cur;
+    
+    int solve(string &s, string &t, vector<vector<int>>&dp, int i, int j){
+        if(j < 0)return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=0;
+        if(s[i] == t[j]){
+            int pick = solve(s,t,dp,i-1,j-1);
+            int notpick = solve(s,t,dp,i-1,j);
+            ans = pick + notpick;
         }
-        return (int)prev[m];
+        else{
+            ans = solve(s,t,dp,i-1,j);
+        }
+        dp[i][j] = ans;
+        return ans;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size(), m=t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(s,t,dp,n-1,m-1);
     }
 };
