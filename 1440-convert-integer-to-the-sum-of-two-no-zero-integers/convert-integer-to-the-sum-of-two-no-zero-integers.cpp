5661a747bf7089ed1bfe887a3fa68e565465947e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-09-08 06:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.27%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        int a=0,b=0,cnt=1;
+        while(n>0){
+            int digit = n%10;
+            n=n/10;
+            if((digit==0 || digit==1) && n>0){
+                a += cnt*(1+digit);
+                b += cnt*9;
+                n--;
+            }else{
+                a += cnt*1;
+                b += cnt*(digit-1);
+            }
+            cnt = cnt * 10;
+        }
+        return {a,b};
+    }
+};
