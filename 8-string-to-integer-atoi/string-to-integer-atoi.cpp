1b8531f9724d047f521363b31a41fff4cbb1418a
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-09-21 16:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (97.46%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int sign = 1, base = 0, i=0;
-        while(s[i] == ' ') i++;//increment for leading whitespaces
-        if(s[i]=='-' || s[i]=='+') sign = 1-2*(s[i++]=='-');//check for sign and assign flag value accordingly
-        while(s[i] >= '0' && s[i] <= '9'){//till it's a digit b/w 0-9...
-            if(base > INT_MAX/10 || (base == INT_MAX/10 && s[i] - '0' > 7)){//check the overflow condition
-                if(sign == 1) return INT_MAX;
-                else return INT_MIN;
-            }
-            base = 10 * base + (s[i++]-'0');// calculate base value for each digit encountered
+        bool neg = 0; 
+        int ans=0, i=0;
+        while(s[i] == ' ')i++;
+        if(s[i]=='-'){
+            neg = 1;
+            i++;
+        } 
+        else if(s[i]=='+'){
+            neg=0;
+            i++;
         }
-        return base * sign;
+        while(s[i] >= '0' && s[i]<='9'){
+            if(ans > INT_MAX/10 || (ans == INT_MAX/10 && s[i] - '0' > 7))
+                return neg ? INT_MIN : INT_MAX;
+            ans = (ans*10) + (s[i]-'0');
+            i++;
+        }
+        return neg ? -ans:ans;
     }
 };
