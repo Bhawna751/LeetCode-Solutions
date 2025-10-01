21c2806387b74a1588fd723a1dea01f12bdabb59
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-01 15:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (49.42%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mpp;
-        int n = nums1.size();
-        for(int it: nums1){
-            mpp[it]++;
-        }
+        unordered_map<int,int>mpp;
+        for(int it:nums1) mpp[it]++;
         vector<int> ans;
         for(int it:nums2){
-           if(mpp.count(it) > 0 && mpp[it]>0){
+            if(mpp.count(it) > 0 && mpp[it]>0){
                 ans.push_back(it);
                 mpp[it]--;
-           }
+            }
         }
         return ans;
     }
