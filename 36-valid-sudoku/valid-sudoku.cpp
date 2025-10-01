114157622937ac31bc536f890d49005d1e81ffd0
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-02 01:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (53.35%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int vis1[9][9] = {0},vis2[9][9] = {0},vis3[9][9] = {0};
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j] != '.'){
-                    int it = board[i][j] - '0' -1;
-                    int k = i/3 * 3+j /3;
+        int vis1[9][9] = {0};
+        int vis2[9][9] = {0};
+        int vis3[9][9] = {0};
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]!='.'){
+                    int it = board[i][j] - '0' - 1;
+                    int k = (i/3) * 3 + (j/3);
                     if(vis1[i][it] || vis2[j][it] || vis3[k][it]) return false;
-                    vis1[i][it] = 1;
-                    vis2[j][it] = 1;
-                    vis3[k][it] = 1;
+                    vis1[i][it]=1;
+                    vis2[j][it]=1;
+                    vis3[k][it]=1;
                 }
             }
         }
