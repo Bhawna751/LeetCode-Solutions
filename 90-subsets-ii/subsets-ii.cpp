b772eb83b6e59d1ea9abdb5549ae0826f219c3f1
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-06 11:54:25 +0530

    Time: 2 ms (41.73%) | Memory: 10.4 MB (84.51%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&subset,int ind){
-        if(nums.size()==ind){
+    void solve(vector<vector<int>> &ans, vector<int>&subset, vector<int>&nums, int ind){
+        if(ind == nums.size()){
             ans.push_back(subset);
             return;
         }
         subset.push_back(nums[ind]);
-        solve(nums,ans,subset,ind+1);
+        solve(ans,subset,nums,ind+1);
         subset.pop_back();
-        for(int j=ind+1;j<nums.size();j++){
-            if(nums[j]!=nums[ind]){
-                solve(nums,ans,subset,j);
+        for(int i=ind+1;i<nums.size();i++){
+            if(nums[i] != nums[ind]){
+                solve(ans,subset,nums,i);
                 return;
             }
         }
-        solve(nums,ans,subset,nums.size());
+        solve(ans,subset,nums,nums.size());
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int>subset;
-        sort(nums.begin(),nums.end());
-        solve(nums,ans,subset,0);
+        int n = nums.size();
+        vector<vector<int>>ans;
+        vector<int> subset;
+        sort(nums.begin(), nums.end());
+        solve(ans,subset,nums,0);
         return ans;
     }
 };
