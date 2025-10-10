4bfb687e5e53f650174d0e1566172fab596f3c26
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-10 12:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (40.60%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int solve( vector<int> &arr){
-        int ind = arr.size();
-        int prev2= 0, prev = arr[0];
-
+    int solve(vector<int>&arr, int ind,vector<int>&dp){
         if(ind ==0)return arr[ind];
-        if(ind<0)return 0;
-        
-        for(int i=1;i<ind;i++){
-            int pick = arr[i];
-            if(i>1)pick += prev2;
-            int notpick = prev;
-            int cur = max(pick ,notpick);
-            prev2 = prev;
-            prev = cur;
-        }
-        return prev;
+        if(ind < 0)return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int pick = solve(arr, ind-2,dp)+arr[ind];
+        int notpick = solve(arr, ind-1,dp);
+        return dp[ind] = max(pick, notpick);
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> arr1, arr2;
-        if(n==1) return nums[0];
+        vector<int>arr1, arr2;
+        int n = nums.size();
+        vector<int>dp1(n,-1), dp2(n,-1);
+        if(n==1)return nums[0];
         for(int i=0;i<n;i++){
             if(i!=n-1)arr1.push_back(nums[i]);
             if(i!=0) arr2.push_back(nums[i]);
-
         }
-        
-        int ans1 = solve(arr1);
-        int ans2  = solve(arr2);
-        return max(ans1, ans2);
+        int ans1 = solve(arr1, arr1.size()-1,dp1);
+        int ans2 = solve(arr2, arr2.size()-1,dp2);
+        return max(ans1,ans2);
     }
 };
