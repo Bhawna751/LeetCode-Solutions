d448b944ffda3cd6758825ef730554a5ed5aa00f
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-10 19:57:13 +0530

    Time: 54 ms (20.37%) | Memory: 48.9 MB (37.99%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>&coins, int ind, int target,vector<vector<int>>&dp){
-        if(ind==0){
-            if(target % coins[ind] == 0) return target / coins[ind];
-            return 1e9;   
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int pick = 1e9;
-        if(coins[ind]<=target) pick = 1 + solve(coins, ind, target-coins[ind],dp); 
-        int notpick = solve(coins, ind-1, target,dp);
-        return dp[ind][target]=min(pick ,notpick);
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = solve(coins, n-1, amount,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)dp[0][i]=i/coins[0];
+            else dp[0][i]=1e9;
+        }
+        for(int ind = 1;ind <n;ind++){
+            for(int target = 0;target<=amount;target++){
+                int pick = 1e9;
+                if(target >= coins[ind])pick = 1 + dp[ind][target-coins[ind]];
+                int notpick = dp[ind-1][target];
+                dp[ind][target] = min(pick,notpick);
+            }
+        }
+        int ans = dp[n-1][amount];
         return ans >= 1e9 ? -1 : ans;
     }
 };
