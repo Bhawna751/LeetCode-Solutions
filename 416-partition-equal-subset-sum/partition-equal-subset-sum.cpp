c9e73aaf3defacb2afae330a09f22f06f45b788d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-11 12:50:02 +0530

    Time: 171 ms (23.11%) | Memory: 21 MB (53.41%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
+    bool solve(vector<int>&nums,int target, int ind,vector<vector<int>>dp){
+        if(target==0)return true;
+        if(ind==0)return nums[ind]==target;
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        bool notpick = solve(nums,target,ind-1,dp);
+        bool pick = false;
+        if(nums[ind]<=target)pick = solve(nums,target-nums[ind],ind-1,dp);
+        return dp[ind][target]=pick || notpick;
+    }
     bool canPartition(vector<int>& nums) {
-        int total=0;
-        int n= nums.size();
-        for(int i=0;i<n;i++){
-            total+= nums[i];
-        }
-        if(total%2==1)return false;
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(),0);
+        if(sum%2==1)return false;
         else{
-            int k = total/2;
-            vector<bool> prev(k+1,false);
-            prev[0]=true;
-            if(nums[0]<=k) prev[nums[0]]=true;
-            for(int ind=1;ind<n;ind++){
-                vector<bool> cur(k + 1, false);
-                cur[0] = true;
-                for (int target = 1; target <= k; target++) {
-                
-                    bool notTaken = prev[target];
-                    bool taken = false;
-                    if (nums[ind] <= target)
-                        taken = prev[target - nums[ind]];
-                        cur[target] = notTaken || taken;
-                }
-                prev = cur;
+            int target = sum/2;
+            vector<vector<bool>> dp(n,vector<bool>(sum+1,0));
+            for(int i=0;i<n;i++){
+                dp[i][0]=1;
             }
-            return prev[k];
+            if(nums[0]<=target) dp[0][nums[0]]=1;
+            for(int i=1;i<n;i++){
+                for(int j=1;j<=target;j++){
+                    bool pick = false;
+                    if(nums[i]<=j)pick = dp[i-1][j-nums[i]];
+                    bool notpick = dp[i-1][j];
+                    dp[i][j] = pick||notpick;
+                }
+            }
+            return dp[n-1][target];
         }
-       
     }
 };
