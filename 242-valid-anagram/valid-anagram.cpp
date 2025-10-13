1f69cad857bfe60f6a8cca46cc28861d14371b7d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-13 23:26:51 +0530

    Time: 2 ms (56.31%) | Memory: 9.9 MB (11.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>mpp;
+        unordered_map<char, int>mpp;
         for(char ch:s)mpp[ch]++;
-        for(char ch:t){
-            mpp[ch]--;
-        }
-        for(auto it:mpp) if(it.second!=0) return false;
+        for(char ch:t)mpp[ch]--;
+        for(auto it:mpp)if(it.second!=0)return false;
         return true;
     }
 };
