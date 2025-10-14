18e8b2c24079e428133dc26f0f46edd51affdf1b
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-14 12:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (57.24%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,65 +1,14 @@
 class Solution {
 public:
-    void lpsArray(string& needle, int m, vector<int>& lps) {
-        // Length of the previous longest prefix suffix
-        int len = 0;
-        // lps[0] is always 0
-        lps[0] = 0;
-        // loop calculates lps[i] for i = 1 to m-1
-        int i = 1;
-        while (i < m) {
-            if (needle[i] == needle[len]) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else // (needle[i] != needle[len])
-            {
-                if (len != 0) {
-                    len = lps[len - 1];
-                } else // if (len == 0)
-                {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-    }
     int strStr(string haystack, string needle) {
-        int m = needle.length(), n = haystack.length();
-
-        // Create lps[] that will hold the longest prefix suffix
-        // values for needletern
-        vector<int> lps(m);
-
-        vector<int> result;
-
-        // Preprocess the needletern (calculate lps[] array)
-        lpsArray(needle, m, lps);
-
-        int i = 0; // index for haystack
-        int j = 0; // index for needle
-        while (i < n) {
-            if (needle[j] == haystack[i]) {
-                j++;
-                i++;
-            }
-
-            if (j == m) {
-
-                return i - j;
-            }
-
-            // mismatch after j matches
-            else if (i < n && needle[j] != haystack[i]) {
-                // Do not match lps[0..lps[j-1]] characters, they will match
-                // anyway
-                if (j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
+        int n = haystack.size(), m=needle.size();
+        for(int i=0;;i++){
+            for(int j=0;;j++){
+                if(j == m)return i;
+                if(i+j==n)return -1;
+                if(haystack[i+j]!=needle[j])break;
+                
             }
         }
-        return -1;
     }
 };
