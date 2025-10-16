4699468da08370a5bfba0a5b43c4829437380933
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-16 15:49:03 +0530

    Time: 3 ms (90.80%) | Memory: 120.6 MB (77.91%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
         int n = nums.size();
-        vector<int> freq(value,0);
+        vector<int> freq(value,0);//  1 2 0 1 0 0 2
         int end=0;
         for(int it:nums){
             freq[(it % value + value) % value]++;
         }
-        for(int i=0;i<value;i++){
-            if(freq[i] < freq[end]) end=i;
+        for(int i=1;i<value;i++){//i=2
+            if(freq[i] < freq[end]) end=i;//end=2
         }
         return value  * freq[end] + end;
     }
