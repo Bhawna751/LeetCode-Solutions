4874546018b2927d2e05e55c45185e8d65ea9b5a
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-10-28 16:39:20 +0530

    Time: 4 ms (68.35%) | Memory: 21.8 MB (94.04%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    bool valid(const vector<int>&nums, int dir, int ind,int nz){
-        
-        vector<int>temp = nums;
-        
-        while(nz > 0 && ind >=0 && ind < nums.size()){
-            if(temp[ind] > 0){
-                temp[ind]--;
-                dir = -dir;
-                if(temp[ind] == 0)nz--;
-            }
-            ind += dir;
-        }
-        return nz == 0;
-    }
     int countValidSelections(vector<int>& nums) {
-        int n = nums.size();
-        int ans=0,nz=0;
-        for(int it:nums)if(it>0)nz++;
+        int n=nums.size();
+        int ans=0, sum = accumulate(nums.begin(), nums.end(),0);
+        int left = 0, right = sum;
         for(int i=0;i<n;i++){
             if(nums[i]==0){
-                if(valid(nums,1,i,nz)) ans++;
-                if (valid(nums,-1,i,nz)) ans++;
+                if(left-right >=0 && left-right <= 1) ans++;
+                if(right-left >=0 && right-left <=1)ans++;
             }
-        }
+            else{
+                left += nums[i];
+                right -= nums[i];
+            }
+        }   
         return ans;
     }
 };
