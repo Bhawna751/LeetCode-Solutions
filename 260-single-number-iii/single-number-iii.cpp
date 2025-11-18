1872964f15595bdd80066584ce7d2a309d3d1d11
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-11-18 11:15:36 +0530

    Time: 4 ms (18.62%) | Memory: 14.9 MB (13.82%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        long xorr =0;
-        for(int i =0; i<nums.size();i++){
-            xorr = xorr^nums[i];
-        }
-        int rightmost =1;
-        while(!(xorr & rightmost)){
-            rightmost = rightmost<<1;
-        }
-        int b1=0,b2=0;
+        vector<int>ans;
+        unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]&rightmost) b1 =  b1^nums[i];
-            else b2 = b2^nums[i];
+            mpp[nums[i]]++;
         }
-        return {b1,b2};
+        for(auto it:mpp){
+            if(it.second==1)ans.push_back(it.first);
+        }
+        return ans;
     }
 };
