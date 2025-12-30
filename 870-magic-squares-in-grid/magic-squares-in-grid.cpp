7338b9acbc0d5bb77a80dc64d75a5579ccb38456
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2025-12-30 23:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (51.37%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,35 +1,57 @@
 class Solution {
 public:
-    bool magic(vector<vector<int>> &grid, int row, int col){
-        bool vis[10] = {false};
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                int it = grid[row + i][col + j];
-                if(it < 1 || it > 9)return false;
-                if(vis[it])return false;
-                vis[it]=true;
+    bool check(vector<vector<int>>& grid, int i, int j)
+    {
+        vector<int> vis(10, 0);
+        for(int r=i; r<i+3; r++)
+        {
+            for(int c=j; c<j+3; c++)
+            {
+                int x = grid[r][c];
+                if(x < 1 || x > 9) return false;
+                if(vis[x]) return false;
+                vis[x] = 1;
             }
         }
-        int diag = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2];
-        int diag2 = grid[row + 2][col] + grid[row+1][col+1] + grid[row][col+2];
-        if(diag != diag2) return false;
-        int r1 = grid[row][col] + grid[row][col+1] + grid[row][col+2];
-        int r2 = grid[row + 1][col] + grid[row + 1][col + 1] + grid[row+1][col+2];
-        int r3 = grid[row + 2][col] + grid[row + 2][col + 1] + grid[row+2][col+2];
-        if(!(r1 == diag && r2 == diag && r3 == diag)) return false;
-        int col1 = grid[row][col] + grid[row + 1][col] + grid[row + 2][col];
-        int col2 = grid[row][col + 1] + grid[row + 1][col + 1] + grid[row + 2][col + 1];
-        int col3 = grid[row][col + 2] + grid[row + 1][col + 2] + grid[row + 2][col + 2];
-        if(!(col1 == diag && col2 == diag && col3 == diag)) return false;
-        return true;
+
+           int rowSum1 = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+           int rowSum2 = grid[i+1][j] + grid[i+1][j+1] + grid[i+1][j+2];
+           int rowSum3 = grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2];
+
+           if(rowSum1 != rowSum2 || rowSum2 != rowSum3) return false;
+           
+           int colSum1 = grid[i][j] + grid[i+1][j] + grid[i+2][j];
+           int colSum2 = grid[i][j+1] + grid[i+1][j+1] + grid[i+2][j+1];
+           int colSum3 = grid[i][j+2] + grid[i+1][j+2] + grid[i+2][j+2];
+
+           if(colSum1 != colSum2 || colSum2 != colSum3) return false;
+
+           int dia1 = grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2];
+           int dia2 = grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2];
+
+           if(dia1 != dia2) return false;
+
+           if(rowSum1 != colSum1 || rowSum1 != colSum2 || rowSum1 != colSum3 || rowSum1 != dia1) return false;
+
+           return true;
     }
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int ans=0, n=grid.size(), m=grid[0].size();
-        for(int i=0;i+2 < n;i++){
-            for(int j=0;j+2<m;j++){
-                if(magic(grid,i,j)) ans++;
+        
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int ans = 0;
+        for(int i=0;i<n-2;i++)
+        {
+            for(int j=0;j<m-2;j++)
+            {
+                if(check(grid, i, j))
+                {
+                    ans++;
+                }
             }
         }
+
         return ans;
     }
 };
