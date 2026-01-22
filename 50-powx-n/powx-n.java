f00b5554d235ff815987d54d8a42f15ec32250d3
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-01-22 10:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (6.91%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,13 +1,13 @@
 class Solution {
-    private double power(double x, long n){
+    public double solve(double x, long n){
         if(n==0)return 1.0;
         if(n==1)return x;
-        if(n%2==0)return power(x*x, n/2);
-        return power(x,n-1)*x;
+        if(n%2==0) return solve(x*x, n/2);
+        return solve(x,n-1)*x;
     }
     public double myPow(double x, int n) {
         long num=n;
-        if(num<0)return 1.0/power(x,-num);
-        return power(x,num);
+        if(num<0)return 1.0/solve(x,-num);
+        return solve(x,num);
     }
 }
