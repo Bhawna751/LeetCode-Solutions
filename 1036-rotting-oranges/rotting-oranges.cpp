d3cecf30d06cc4f9c03185ce9b73133f392b8a50
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-01-31 13:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (52.23%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-     int n=grid.size(), m=grid[0].size();
-     vector<vector<int>> vis(n,vector<int>(m,-1));
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,-1));
         int delrow[4] = {0,1,0,-1};
         int delcol[4] = {1,0,-1,0};
-        int total = 0, cnt =0, time=0;
-        queue<pair<int,int>> q;
+        int total = 0, cnt=0, time=0;
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == 2) q.push({i,j});
-                if(grid[i][j] == 1 || grid[i][j] == 2) total++;
+                if(grid[i][j]==1 || grid[i][j]==2) total++;
+
             }
         }
         while(!q.empty()){
@@ -20,16 +21,16 @@ public:
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
-                for(int d = 0;d<4;d++){
-                    int nx = delrow[d] + x;
-                    int ny = delcol[d] + y;
-                    if(nx < 0 || nx >= n || ny < 0 || ny >=m  || grid[nx][ny]!=1) continue;
+                for(int d=0;d<4;d++){
+                    int nx= x + delrow[d];
+                    int ny = y + delcol[d];
+                    if(nx<0 || ny <0 || nx >= n || ny >= m || grid[nx][ny]!=1)continue;
                     grid[nx][ny] = 2;
                     q.push({nx,ny});
                 }
             }
             if(!q.empty()) time++;
-         }
-         return total == cnt ?  time : -1;
+        }
+        return total == cnt ? time : -1;
     }
 };
