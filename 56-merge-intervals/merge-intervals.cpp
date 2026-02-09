f698b361adc2631506116d504e91931c2dbb7d82
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-02-09 17:56:37 +0530

    Time: 7 ms (40.41%) | Memory: 23.9 MB (58.78%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,8 +4,8 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        for(int i =1;i<intervals.size();i++){
-            if(ans.back()[1] >= intervals[i][0]){
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=ans.back()[1]){
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
             else ans.push_back(intervals[i]);
