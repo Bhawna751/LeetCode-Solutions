8bac54c409bff5ed897999f6708b7b3abd4be540
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-02-09 18:33:56 +0530

    Time: 76 ms (59.38%) | Memory: 173.4 MB (51.49%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,62 +1,73 @@
+class Node{
+public:
+    int key, val;
+    Node *next, *prev;
+    Node(){
+        key = -1;
+        val = -1;
+        next = nullptr;
+        prev = nullptr;
+
+    }
+    Node(int k, int value){
+        key = k;
+        val= value;
+        next=prev=nullptr;
+    }
+};
 class LRUCache {
 public:
-    class ListNode {
-        public:
-            int key,val;
-            ListNode *next,*prev;
-            ListNode(int k,int v){
-                key=k;
-                val=v;
-            }
-    };
-    ListNode *head = new ListNode(-1,-1);
-    ListNode *tail = new ListNode(-1,-1);
-    int c;
-    unordered_map<int,ListNode*>mpp;
-    LRUCache(int capacity) {
-        c = capacity;
-        head->next = tail;
-        tail->prev = head;
+    map<int,Node*> mpp;
+    int cap;
+    Node *head, *tail;
+    void deleteNode(Node* node){
+        Node* p=node->prev;
+        Node* n = node->next;
+        p->next = n;
+        n->prev = p;
     }
-    void add(ListNode* node){
-        ListNode* temp = head->next;
-        node->next = temp;
-        node->prev = head;
+    void insertNode(Node* node){
+        Node *n=head->next;
         head->next = node;
-        temp->prev = node;
+        n->prev = node;
+        node->prev = head;
+        node->next = n;
     }
-    void remove(ListNode *node){
-        ListNode *temp = node->prev;
-        ListNode *q = node->next;
-        temp->next = q;
-        q->prev = temp;
-        
+    LRUCache(int capacity) {
+        cap = capacity;
+        mpp.clear();
+        head = new Node();
+        tail = new Node();
+        head->next = tail;
+        tail->prev=head;
     }
+    
     int get(int key) {
-        if(mpp.find(key)!=mpp.end()){
-            ListNode* node = mpp[key];
-            int res =node->val;
-            mpp.erase(key);
-            remove(node);
-            add(node);
-            mpp[key]=head->next;
-            return res;
-        }
-        return -1;
+        if(mpp.find(key) == mpp.end()) return -1;
+        Node *node = mpp[key];
+        int val =node->val;
+        deleteNode(node);
+        insertNode(node);
+        return val;
     }
     
     void put(int key, int value) {
         if(mpp.find(key)!=mpp.end()){
-            ListNode * node = mpp[key];
-            mpp.erase(key);
-            remove(node);
+            Node *node = mpp[key];
+            node->val = value;
+            deleteNode(node);
+            insertNode(node);
+            return;
         }
-        if(mpp.size() == c){
-            mpp.erase(tail->prev->key);
-            remove(tail->prev);
+
+        if(mpp.size()==cap){
+            Node *node = tail->prev;
+            mpp.erase(node->key);
+            deleteNode(node);
         }
-        add(new ListNode(key,value));
-        mpp[key] = head->next;
+        Node* n=new Node(key,value);
+        mpp[key]=n;
+        insertNode(n);
     }
 };
 
