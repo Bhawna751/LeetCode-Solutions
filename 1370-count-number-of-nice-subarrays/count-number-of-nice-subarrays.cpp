607dfced2c4ee8697fbad18cb6a12eacd374c724
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-03-13 21:15:48 +0530

    Time: 6 ms (71.42%) | Memory: 71.4 MB (94.20%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int ans = 0, n=nums.size();//1 1 2 1 1    n = 5
-        int odd = 0,cnt=0;
-        int low = 0,high=0;
-        while(high < n){// 4 < 5
-            if(nums[high]%2 != 0){
-                cnt=0;//cnt = 0
-                odd++;//odd = 3
+        int n = nums.size();
+        int ans=0, cnt=0;
+        int l=0,h=0;
+        int odd=0;
+        while(h<n){
+            if(nums[h]%2!=0){
+                cnt=0;
+                odd++;
             }
-            while(odd == k){
-                cnt++;//cnt = 2
-                odd -= nums[low]&1;//odd = 3 - 
-                low++;//low = 1
+            while(odd==k){
+                cnt++;
+                odd -= (nums[l] % 2);
+                l++;
             }
-            ans += cnt;//ans = 1
-            high++;//high = 4
+            ans += cnt;
+            h++;//3
         }
         return ans;
     }
