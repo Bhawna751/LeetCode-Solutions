9724b2a1ede41e2d2f3e4dfcc5f3acda7b29d636
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-03-14 21:39:02 +0530

    Time: 11 ms (23.01%) | Memory: 19.6 MB (46.94%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool cycle(vector<int> adj[], vector<int>&vis, int node){
-        if(vis[node] == 1)return true;
+    bool cycle(vector<int>adj[], vector<int>&vis, int node){
+        if(vis[node]==1)return true;
         if(vis[node]==0){
             vis[node]=1;
-            for(auto it: adj[node]){
+            for(auto it:adj[node]){
                 if(cycle(adj, vis, it)) return true;
             }
         }
-        vis[node]=2;
+        vis[node] = 2;
         return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
-        for(auto it: prerequisites){
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int>adj[n];
+        for(auto it:prerequisites){
             adj[it[1]].push_back(it[0]);
+            
         }
-        vector<int> vis(numCourses, 0);
-        for(int i=0;i<numCourses;i++){
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
             if(cycle(adj, vis, i)) return false;
         }
         return true;
