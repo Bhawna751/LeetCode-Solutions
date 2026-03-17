b4ff844d082d5062963db9c27ac5e6b028174010
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2026-03-17 08:25:01 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (50.21%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-    int f(int ind, int buy, vector<int>& prices, int n,
-          vector<vector<int>>& dp) {
-        if (ind == n)
-            return 0;
-        if (dp[ind][buy] != -1)
-            return dp[ind][buy];
-        int profit = 0;
-        if (buy) {
-            profit = max(-prices[ind] + f(ind + 1, 0, prices, n, dp),
-                         0 + f(ind + 1, 1, prices, n, dp));
-        } else {
-            profit = max(prices[ind] + f(ind + 1, 1, prices, n, dp),
-                         0 + f(ind + 1, 0, prices, n, dp));
-        }
-        return dp[ind][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<long>ahead(2,0), cur(2, 0);
-        ahead[0] = ahead[1] = 0;
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int b = 0; b <= 1; b++) {
-                long profit = 0;
-                if (b) {
-                    profit = max(-prices[ind] + ahead[0],ahead[ 1]);
-                } else {
-                    profit = max(prices[ind] + ahead[1], ahead[0]);
-                }
-                 cur[b] = profit;
+        int ans=0;
+        if(n<2)return 0;
+        for(int i=0,j=1;j<n;j++){
+            if(prices[i] < prices[j]){
+                while(j+1<n && prices[j]<prices[j+1])j++;
+                ans += prices[j]-prices[i];
             }
-            ahead=cur;
+            i=j;
         }
-        return ahead[1];
+        return ans;
     }
 };
